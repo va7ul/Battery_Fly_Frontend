d792d88be4274595175f9f781f4e7306f5605347
Va7ul <vas7ul@gmail.com>
2024-02-18 18:31:25 +0200

    Continue fixing

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,8 @@
 import { Suspense } from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
-import { Container } from '../layout/Layout.styled';
+import { Outlet } from 'react-router-dom';
+import { Container } from './Layout.styled';
 import { Footer } from './footer/Footer';
+import { Header } from './header/Header';
 
 // import { MenuAppBar } from 'components/Appbar/AppBar';
 
@@ -9,13 +10,7 @@ export const Layout = () => {
   return (
     <>
       <header>
-        <NavLink to="/">Головна</NavLink>
-        <NavLink to="/about">Про нас</NavLink>
-        <NavLink to="/assortment">Асортимент</NavLink>
-        <NavLink to="/delivery-and-payment">Доставка та оплата</NavLink>
-        <NavLink to="/contacts">Контакти</NavLink>
-        {/* <MenuAppBar /> */}
-        <hr />
+        <Header />
       </header>
       <main>
         <section>
--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,3 +1,14 @@
+import { NavLink } from 'react-router-dom';
+
 export const Header = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <NavLink to="/">Головна</NavLink>
+      <NavLink to="/about">Про нас</NavLink>
+      <NavLink to="/assortment">Асортимент</NavLink>
+      <NavLink to="/delivery-and-payment">Доставка та оплата</NavLink>
+      <NavLink to="/contacts">Контакти</NavLink>
+      <hr />
+    </>
+  );
 };
