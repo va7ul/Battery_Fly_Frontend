d4481eedf7b95f0e8a5b726455525f36785e8426
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-02-19 15:59:27 +0200

    fix

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -8,24 +8,17 @@ export const Contacts = () => {
             <TextTel>Телефонуйте:</TextTel>
             <ul>
                 <li>
-                    <Tel href="tel:+380634350137"
-                >+38 (063) 435 01 37</Tel>
+                    <Tel>+38 (063) 435 01 37</Tel>
                 </li>
                 <li>
-                    <Tel href="tel:+380634350137"
-                >+38 (063) 435 01 37</Tel>
+                    <Tel>+38 (063) 435 01 37</Tel>
                 </li>
             </ul>
             <Text>Пишіть</Text>
-            <Desc href="mailto:batteryfly@gmail.com"
-            >batteryfly@gmail.com</Desc>
+            <Desc>batteryfly@gmail.com</Desc>
             <SocialMedia />
             <Text>Адреса точки видачі</Text>
-              <Desc
-                // href="https://goo.gl/maps/hResCekDZCyXk1Gw8"
-                target="_blank"
-                rel="noopener noreferrer nofollow"
-                >м. Львів вул. Китайська, 14</Desc>
+              <Desc>м. Львів вул. Китайська, 14</Desc>
         </Container>
     )
 }
