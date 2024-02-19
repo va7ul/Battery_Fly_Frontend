23032c4e7145cb82cc8fabc43718d77601aaf926
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-02-19 18:29:13 +0200

    add_sprite

--- src/components/Contacts/ SocialMedia/ SocialMedia.jsx
+++ src/components/Contacts/ SocialMedia/ SocialMedia.jsx
@@ -1,21 +1,25 @@
+import { FaViber } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTelegramPlane } from "react-icons/fa";
 import { List } from "./SocialMedia.styled"
 
 export const SocialMedia = () => { 
     return (
         <List>
                <li>
-              {/* <a> */}
-              insta
+                {/* <a> */}
+                <FaInstagram />
+             
               {/* </a> */}
             </li>
               <li>
               {/* <a> */}
-            viber
+             <FaViber />
               {/* </a> */}
             </li>
               <li>
               {/* <a> */}
-              telega
+              <FaTelegramPlane />
               {/* </a> */}
             </li>
         </List>
--- src/components/Contacts/Contacts.styled.js
+++ src/components/Contacts/Contacts.styled.js
@@ -12,41 +12,26 @@ export const Container = styled.div`
 
 export const Title = styled.h2`
   color: rgb(244, 170, 0);
-  font-family: 'Inter', sans-serif;
   font-size: 16px;
-  font-weight: 600;
-  line-height: 19px;
   margin-bottom: 8px;
 `;
 
 export const TextTel = styled.p`
-  font-family: Inter;
   font-size: 12px;
-  font-weight: 400;
-  line-height: 15px;
   margin-bottom: 13px;
 `;
 
 export const Text = styled.p`
-  font-family: Inter;
   font-size: 12px;
-  font-weight: 400;
-  line-height: 15px;
   margin-bottom: 9px;
   margin-top: 12px;
 `;
 
 export const Tel = styled.a`
-  font-family: Inter;
   font-size: 10px;
-  font-weight: 400;
-  line-height: 12px;
 `;
 
 export const Desc = styled.a`
-  font-family: Inter;
   font-size: 10px;
-  font-weight: 400;
-  line-height: 12px;
   margin-bottom: 9px;
 `;
