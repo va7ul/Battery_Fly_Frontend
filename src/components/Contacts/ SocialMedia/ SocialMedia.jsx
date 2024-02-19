1a3d665fbc94fb6fee65a54fe6bca0bb2a5c1211
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-02-19 15:33:16 +0200

    add_SocialMedia_component

---
+++ src/components/Contacts/ SocialMedia/ SocialMedia.jsx
@@ -0,0 +1,21 @@
+export const SocialMedia = () => { 
+    return (
+        <ul>
+               <li>
+              <a href="">
+              insta
+              </a>
+            </li>
+              <li>
+              <a href="">
+            viber
+              </a>
+            </li>
+              <li>
+              <a href="">
+              telega
+              </a>
+            </li>
+        </ul>
+    )
+}
