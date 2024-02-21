eb276afd0f4d6c3046f250f94158dbf9ea01b47b
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-02-21 16:29:23 +0200

    start_footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,22 @@
-import { Container } from "./Footer.styled";
+import { Box, Container, Logo, Text, Link, Title } from "./Footer.styled";
+import logo from '../../assets/images/logo.png'
 
 export const Footer = () => {
   return (
     <Container>
-      ...footer in development
+      <Logo src={logo} alt='logo' />
+      <Text>Замовлення по телефону <a href="tel:+380931430524">+38 (093) 143-05-24</a><br/>&copy; 2023 batteryfly.ua
+      </Text>
+      <Text>Політика конфіденційності
+      </Text>
+      <Box>
+        <Title>Клієнтам</Title>
+        <Link to='/about'>Про нас</Link>
+        <Link to='/assortment'>Асортимент</Link>
+        <Link to='/delivery-and-payment'>Доставка та оплата</Link>
+        <Link to='/'>Гарантії</Link>
+        <Link to='/contacts'>Контакти</Link>
+      </Box>
     </Container>
   );
 };
--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 export const Container = styled.div`
   padding: 25px 30px;
@@ -8,5 +9,47 @@ export const Container = styled.div`
   @media screen and (min-width: 1280px) {
     padding: 56px 110px;
     max-width: 1280px;
+  }
+`;
+
+export const Logo = styled.img`
+  width: 78px;
+  height: 24px;
+  margin-bottom: 10px;
+`;
+
+export const Text = styled.p`
+  font-size: 10px;
+  margin-bottom: 10px;
+
+  @media screen and (min-width: 1280px) {
+    font-size: 15px;
+  }
+`;
+
+export const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+`;
+
+export const Title = styled.h4`
+  font-size: 10px;
+  font-weight: 600;
+
+  @media screen and (min-width: 1280px) {
+    font-size: 15px;
+  }
+`;
+
+export const Link = styled(NavLink)`
+  font-size: 10px;
+
+  &:hover {
+    color: ${props => props.theme.colors.secondary};
+  }
+
+  @media screen and (min-width: 1280px) {
+    /* font-size: var(--main-font-size); */
   }
 `;
