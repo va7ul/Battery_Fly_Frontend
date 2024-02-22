93df26f006d49a217d5eab192828d431c6835bde
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-02-22 12:29:34 +0200

    add_css

---
+++ src/components/SocialMedia/SocialMedia.styled.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+  display: flex;
+  gap: 6px;
+  /* margin-bottom: 12px; */
+
+  @media screen and (min-width: 1280px) {
+    /* margin-bottom: 36px; */
+  }
+`;
+
+export const Item = styled.li`
+  /* color: ${props => props.theme.colors.secondary};
+  opacity: 0.7; */
+  transition: 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  cursor: pointer;
+  display: block;
+  height: 20px;
+
+  &:hover {
+    opacity: 1;
+    color: ${props => props.theme.colors.secondary};
+    transform: scale(1.05);
+  }
+
+  @media screen and (min-width: 1280px) {
+    height: 24px;
+  }
+`;
