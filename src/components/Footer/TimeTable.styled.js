de642a532bb49896313ad95e2fa690b6340b307d
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-02-22 12:29:46 +0200

    add_css

---
+++ src/components/Footer/TimeTable.styled.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+`;
+
+export const Title = styled.h4`
+  font-size: 10px;
+  font-weight: 600;
+
+  @media screen and (min-width: 1280px) {
+    font-size: 15px;
+  }
+`;
+
+export const Text = styled.p`
+  font-size: 10px;
+
+  @media screen and (min-width: 1280px) {
+    font-size: 15px;
+  }
+`;
