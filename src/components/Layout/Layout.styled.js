d7ae604fd1049a414160e75519d606816d9bde80
1evanter <rita.oleksenkoo@gmail.com>
2024-02-23 17:10:30 +0200

    Update Layout.styled.js

--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -15,3 +15,24 @@ export const Container = styled.div`
 export const AppTitle = styled.h1`
   ${hidden}
 `;
+
+export const StyledHeader = styled.header`
+  position: sticky;
+  left: 0;
+  top: 0;
+  display: flex;
+  max-width: 390px;
+  height: 100vh;
+  z-index: 1;
+
+  background-color: ${props => props.theme.colors.background};
+
+  @media screen and (min-width: 1280px) {
+    padding: 15px 20px;
+    margin: 0 auto;
+    align-items: center;
+    height: 64px;
+    max-width: 1280px;
+    padding: 20px 100px;
+  }
+`;
