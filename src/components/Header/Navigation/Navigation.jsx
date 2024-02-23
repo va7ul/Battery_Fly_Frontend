51ddd6a9ee59bd9df7097741c174baa738870a58
1evanter <rita.oleksenkoo@gmail.com>
2024-02-23 17:10:06 +0200

    Update Navigation.jsx

--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -1,12 +1,25 @@
-import { StyledLink } from './Navigation.styled';
+import { StyledLink, Nav, NavList } from './Navigation.styled';
 
 export const Navigation = () => {
   return (
-    <>
-      <StyledLink to="/about">Про нас</StyledLink>
-      <StyledLink to="/assortment">Асортимент</StyledLink>
-      <StyledLink to="/delivery-and-payment">Доставка та оплата</StyledLink>
-      <StyledLink to="/contacts">Контакти</StyledLink>
-    </>
+    <Nav>
+      <NavList>
+        <li>
+          <StyledLink to="/about">Про нас</StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/assortment">Асортимент</StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/delivery-and-payment">Доставка та оплата</StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/contacts">Контакти</StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/contacts">Кошик</StyledLink>
+        </li>
+      </NavList>
+    </Nav>
   );
 };
