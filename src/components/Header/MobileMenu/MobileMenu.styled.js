c027e1e1e05ae9ee9fe796045fbcab55bd18251d
1evanter <rita.oleksenkoo@gmail.com>
2024-02-23 17:10:19 +0200

    Update MobileMenu.styled.js

--- src/components/Header/MobileMenu/MobileMenu.styled.js
+++ src/components/Header/MobileMenu/MobileMenu.styled.js
@@ -1,3 +1,16 @@
 import styled from 'styled-components';
 
-export const Logo = styled.img``;
+export const Logo = styled.img`
+  width: 97px;
+
+  @media screen and (min-width: 1280px) {
+    width: 112px;
+  }
+`;
+
+export const Button = styled.button`
+  width: 24px;
+  background-color: transparent;
+  outline: none;
+  border: none;
+`;
