f0d53ab2da387674b6ac6a8b137972cd8cb94d0e
1evanter <rita.oleksenkoo@gmail.com>
2024-02-23 21:22:37 +0200

    Update Navigation.styled.js

--- src/components/Header/Navigation/Navigation.styled.js
+++ src/components/Header/Navigation/Navigation.styled.js
@@ -1,20 +1,23 @@
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
-export const StyledLink = styled(NavLink)`
+export const Item = styled.li`
   font-size: 15px;
+  color: ${props => props.theme.colors.textPrimary};
   transition: color ${props => props.theme.transition.main};
 
   &:hover {
     color: ${props => props.theme.colors.secondary};
   }
 
-  &.active {
-    color: ${props => props.theme.colors.secondary};
-  }
-
   @media screen and (min-width: 1280px) {
     font-weight: 600;
+  }
+`;
+
+export const StyledLink = styled(NavLink)`
+  &.active {
+    color: ${props => props.theme.colors.secondary};
   }
 `;
 
