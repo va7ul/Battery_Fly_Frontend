3c8c6d8fb73a0e15b2acac8e3eb44de28be9b244
Va7ul <vas7ul@gmail.com>
2024-02-23 16:58:37 +0200

    Create basic markup of component card

--- src/components/AssortmentPage/SaleSection/SaleList/SaleCard/SaleCard.jsx
+++ src/components/AssortmentPage/SaleSection/SaleList/SaleCard/SaleCard.jsx
@@ -4,7 +4,46 @@ export const SaleCard = () => {
   return (
     <>
       <StyledImage src="" alt="text" />
-      <div>Card</div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          marginTop: '15px',
+        }}
+      >
+        <p style={{ textAlign: 'center', marginBottom: '10px' }}>
+          Lorem ipsum dolor sit amet consectetur
+        </p>
+        <span style={{ fontSize: '24px', fontWeight: 600 }}>15000 грн</span>
+        <span
+          style={{
+            textDecorationLine: 'line-through',
+            fontSize: '24px',
+            color: 'rgba(157, 157, 157, 1)',
+            marginBottom: '20px',
+          }}
+        >
+          18000 грн
+        </span>
+        <button
+          style={{
+            marginBottom: '10px',
+          }}
+        >
+          - 1 шт +
+        </button>
+        <button
+          style={{
+            padding: '6px 18px',
+            border: 'transparent',
+            borderRadius: '15px',
+            background: 'rgba(255, 224, 153, 1)',
+          }}
+        >
+          В кошик
+        </button>
+      </div>
     </>
   );
 };
