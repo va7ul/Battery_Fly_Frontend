e03df2a0a97fc5f2276eb59480bddba342cd51bb
1evanter <rita.oleksenkoo@gmail.com>
2024-02-23 17:10:35 +0200

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,7 +2,7 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import { themeMUI } from 'styles/GlobalStyled';
-import { Container } from './Layout.styled';
+import { Container, StyledHeader } from './Layout.styled';
 import { Footer } from '../Footer/Footer';
 import { Header } from '../../components/Header/Header';
 
@@ -12,9 +12,9 @@ export const Layout = () => {
   return (
     <>
       <ThemeProvider theme={themeMUI}>
-        <header>
+        <StyledHeader>
           <Header />
-        </header>
+        </StyledHeader>
         <main>
           <section>
             <Container>
