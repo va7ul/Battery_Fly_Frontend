7450d78dbd07cfb7e4bd4b401cad0545312ce2cd
1evanter <rita.oleksenkoo@gmail.com>
2024-02-24 19:03:30 +0200

    move header to Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,13 @@ import { useMediaQuery } from 'react-responsive';
 import { Navigation } from './Navigation/Navigation';
 import { Logo } from './MobileMenu/MobileMenu.styled';
 import logo from '../../assets/images/logo.png';
+import { StyledHeader } from './Header.styled';
 //  jjj
 export const Header = () => {
   const mobileVersion = useMediaQuery({ query: '(max-width:1279px)' });
 
   return (
-    <>
+    <StyledHeader>
       {mobileVersion ? (
         <MobileMenu />
       ) : (
@@ -18,6 +19,6 @@ export const Header = () => {
       )}
 
       {/* <NavLink to="/">+38 (063) 435 01 37</NavLink> */}
-    </>
+    </StyledHeader>
   );
 };
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,7 +2,7 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import { themeMUI } from 'styles/GlobalStyled';
-import { Container, StyledHeader } from './Layout.styled';
+import { Container } from './Layout.styled';
 import { Footer } from '../Footer/Footer';
 import { Header } from '../../components/Header/Header';
 
@@ -12,9 +12,7 @@ export const Layout = () => {
   return (
     <>
       <ThemeProvider theme={themeMUI}>
-        <StyledHeader>
-          <Header />
-        </StyledHeader>
+        <Header />
         <main>
           <Container>
             <Suspense fallback={null}>
