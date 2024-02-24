dfe2747ea63c562eabd09899e1f2d0a4562a1001
1evanter <rita.oleksenkoo@gmail.com>
2024-02-24 19:03:45 +0200

    move Header's styles

---
+++ src/components/Header/Header.styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const StyledHeader = styled.header`
+  position: sticky;
+  left: 0;
+  top: 0;
+  display: flex;
+  max-width: 390px;
+  z-index: 1;
+
+  background-color: ${props => props.theme.colors.background};
+
+  @media screen and (min-width: 1280px) {
+    padding: 15px 20px;
+    margin: 0 auto;
+    align-items: center;
+    height: 64px;
+    max-width: 1280px;
+    padding: 20px 100px;
+  }
+`;
--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -16,23 +16,3 @@ export const Container = styled.div`
 export const AppTitle = styled.h1`
   ${hidden}
 `;
-
-export const StyledHeader = styled.header`
-  position: sticky;
-  left: 0;
-  top: 0;
-  display: flex;
-  max-width: 390px;
-  z-index: 1;
-
-  background-color: ${props => props.theme.colors.background};
-
-  @media screen and (min-width: 1280px) {
-    padding: 15px 20px;
-    margin: 0 auto;
-    align-items: center;
-    height: 64px;
-    max-width: 1280px;
-    padding: 20px 100px;
-  }
-`;
