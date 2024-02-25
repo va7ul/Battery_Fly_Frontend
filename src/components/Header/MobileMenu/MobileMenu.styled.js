1285b71ad44507d5db1d8d3276702164978dba81
1evanter <rita.oleksenkoo@gmail.com>
2024-02-25 19:56:44 +0200

    move FeedbackButton

--- src/components/Header/MobileMenu/MobileMenu.styled.js
+++ src/components/Header/MobileMenu/MobileMenu.styled.js
@@ -19,7 +19,6 @@ export const ArrowButton = styled.button`
 export const HeaderWrap = styled.div`
   display: flex;
   align-items: center;
-  padding: 15px 24px;
 `;
 
 export const MenuWrap = styled.div`
@@ -27,18 +26,4 @@ export const MenuWrap = styled.div`
   justify-content: space-between;
   margin: 0 auto;
   padding: 16px 0;
-`;
-
-export const FeedbackButton = styled.button`
-  display: flex;
-  align-items: center;
-  padding: 8px 18px;
-  background-image: ${props => props.theme.colors.gradient};
-  border-radius: 6px;
-  border: none;
-  transition: background-image 250ms cubic-bezier(0.4, 0, 0.2, 1);
-  font-size: 10px;
-  &:hover {
-    background-image: ${props => props.theme.colors.gradientHover};
-  }
 `;
