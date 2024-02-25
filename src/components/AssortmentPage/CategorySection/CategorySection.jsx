f3ea271a379eec3bebfaa36580ecffba50489dbe
Va7ul <vas7ul@gmail.com>
2024-02-26 01:24:26 +0200

    Create links with transitions

--- src/components/AssortmentPage/CategorySection/CategorySection.jsx
+++ src/components/AssortmentPage/CategorySection/CategorySection.jsx
@@ -1,15 +1,21 @@
 import { CategoryList } from './CategoryList/CategoryList';
+import sprite from '../../../assets/images/sprite.svg';
 import {
-  StyledTitle,
+  StyledLink,
   StyledSection,
   StyledContainer,
 } from './CategorySection.styled';
 
 export const CategorySection = ({ category }) => {
   return (
-    <StyledSection>
-      <StyledContainer>
-        <StyledTitle>{category}</StyledTitle>
+    <StyledSection category={category}>
+      <StyledContainer category={category}>
+        <StyledLink href={category} category={category}>
+          <h2>{category}</h2>
+          <svg>
+            <use href={`${sprite}#icon-arrow-right`} />
+          </svg>
+        </StyledLink>
         <CategoryList />
       </StyledContainer>
     </StyledSection>
