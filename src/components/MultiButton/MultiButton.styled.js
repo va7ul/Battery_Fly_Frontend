345535229a925ae5e43fde74673981b2c33680ae
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-02-25 18:26:14 +0200

    add_css_for_multibutton

---
+++ src/components/MultiButton/MultiButton.styled.js
@@ -0,0 +1,94 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  position: fixed;
+  right: 30px;
+  bottom: 50px;
+  display: flex;
+  gap: 12px;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+  gap: 6px;
+  transition: 0.3s ease;
+`;
+
+export const Item = styled.li`
+  border-radius: 50%;
+  cursor: pointer;
+  width: 44px;
+  height: 44px;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
+
+export const Insta = styled.li`
+  background: radial-gradient(
+    circle at 30% 107%,
+    #fdf497 0%,
+    #fdf497 5%,
+    #fd5949 45%,
+    #d6249f 60%,
+    #285aeb 90%
+  );
+  border-radius: 50%;
+  cursor: pointer;
+  width: 44px;
+  height: 44px;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
+
+export const Icon = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  width: 44px;
+  height: 44px;
+  color: white;
+  &:hover {
+    color: ${props => props.theme.colors.textPrimary};
+  }
+`;
+
+export const Button = styled.div`
+  width: 70px;
+  height: 70px;
+  background: ${props => props.theme.colors.secondary};
+  border-radius: 50%;
+  display: flex;
+  cursor: pointer;
+  justify-content: center;
+  align-items: center;
+  color: ${props => props.theme.colors.background};
+  animation: animation_button 3s linear infinite;
+  transition: all 200ms;
+
+  @keyframes animation_button {
+    0% {
+      box-shadow: 0 0 0 0 #ffba26, 0 0 0 0 rgba(255, 206, 9, 0.7);
+    }
+    40% {
+      box-shadow: 0 0 0 13.62667px rgba(255, 186, 38, 0),
+        0 0 0 6.81333px rgba(255, 206, 9, 0.7);
+    }
+    80% {
+      box-shadow: 0 0 0 27.25333px rgba(255, 186, 38, 0),
+        0 0 0 13.62667px rgba(255, 206, 9, 0);
+    }
+    100% {
+      box-shadow: 0 0 0 34.06667px rgba(255, 186, 38, 0),
+        0 0 0 27.25333px rgba(255, 206, 9, 0);
+    }
+  }
+`;
