1cc670cbbd3aed22528795a403d2af367b94a554
1evanter <rita.oleksenkoo@gmail.com>
2024-02-25 15:16:31 +0200

    change logo's size

--- src/components/Header/Logo/Logo.styled.js
+++ src/components/Header/Logo/Logo.styled.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 export const StyledLogo = styled.img`
-  width: 97px;
+  width: 112px;
 
   @media screen and (min-width: 1280px) {
-    width: 112px;
+    width: 177px;
   }
 `;
