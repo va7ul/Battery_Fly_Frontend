94c622c14b325e8cdfec42ad21b9292301049342
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-02-26 13:56:02 +0200

    added_catalog_for_footer_desktop

---
+++ src/components/Footer/Catalog/Catalog.jsx
@@ -0,0 +1,16 @@
+import { Box, Title, Link } from "./Catalog.styled";
+
+export const Catalog = () => {
+    return (
+        <Box>
+            <Title>Каталог</Title>
+            <Link to='/batteries'>Акумулятори</Link>
+            <Link to='/assembly'>Готові батареї</Link>
+            <Link to='/batteries-for-fpv'>Батареї для FPV-дронів</Link>
+            <Link to='/batteries-for-transport'>Батареї для електротранспорту</Link>
+            <Link to='/batteries-for-toys'>Батареї для іграшок</Link>
+            <Link to='/devices'>Прилади</Link>
+            <Link to='/materials'>Розхідні матеріали</Link>
+        </Box>
+    );
+};
