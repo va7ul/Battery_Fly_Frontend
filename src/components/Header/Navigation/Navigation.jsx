66640de088bd16a5bae558ef349c8b2f1434df56
1evanter <rita.oleksenkoo@gmail.com>
2024-02-27 15:15:50 +0200

    Update Navigation.jsx

--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -1,25 +1,54 @@
-import { StyledLink, Nav, NavList, Item } from './Navigation.styled';
+import { StyledLink, NavList, Item, HopeIcon } from './Navigation.styled';
+import { useMediaQuery } from 'react-responsive';
+import sprite from '../../../assets/images/sprite.svg';
 
 export const Navigation = ({ setIsOpen }) => {
+  const mobileVersion = useMediaQuery({ query: '(max-width:1279px)' });
+
   return (
-    <Nav>
+    <nav>
       <NavList onClick={() => setIsOpen(false)}>
         <Item>
+          {mobileVersion && (
+            <HopeIcon>
+              <use href={`${sprite}#icon-Hope`}></use>
+            </HopeIcon>
+          )}
           <StyledLink to="/main">Головна</StyledLink>
         </Item>
         <Item>
+          {mobileVersion && (
+            <HopeIcon>
+              <use href={`${sprite}#icon-Hope`}></use>
+            </HopeIcon>
+          )}
           <StyledLink to="/about">Про нас</StyledLink>
         </Item>
         <Item>
+          {mobileVersion && (
+            <HopeIcon>
+              <use href={`${sprite}#icon-Hope`}></use>
+            </HopeIcon>
+          )}
           <StyledLink to="/assortment">Асортимент</StyledLink>
         </Item>
         <Item>
+          {mobileVersion && (
+            <HopeIcon>
+              <use href={`${sprite}#icon-Hope`}></use>
+            </HopeIcon>
+          )}
           <StyledLink to="/delivery-and-payment">Доставка та оплата</StyledLink>
         </Item>
         <Item>
+          {mobileVersion && (
+            <HopeIcon>
+              <use href={`${sprite}#icon-Hope`}></use>
+            </HopeIcon>
+          )}
           <StyledLink to="/contacts">Контакти</StyledLink>
         </Item>
       </NavList>
-    </Nav>
+    </nav>
   );
 };
