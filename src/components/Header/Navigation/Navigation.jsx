7fe52ca1c900eb79c061b5a7e60522952183f4fc
1evanter <rita.oleksenkoo@gmail.com>
2024-02-29 22:00:01 +0200

    Update Navigation.jsx

--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -1,14 +1,13 @@
 import {
-  // StyledLink,
   NavList,
-  Item,
-  HopeIcon,
   CartButton,
   // ArrowButton,
 } from './Navigation.styled';
+import { Item, HopeIcon } from './NavItem/NavItem.styled';
 import { useMediaQuery } from 'react-responsive';
 import sprite from '../../../assets/images/sprite.svg';
 import { NavItem } from './NavItem/NavItem';
+import { Assortment } from './Assortment/Assortment';
 
 export const Navigation = ({ setIsOpen }) => {
   const mobileVersion = useMediaQuery({ query: '(max-width:1279px)' });
@@ -18,7 +17,7 @@ export const Navigation = ({ setIsOpen }) => {
       <NavList onClick={() => setIsOpen(false)}>
         <NavItem page="/main" title="Головна" />
         <NavItem page="/about" title="Про нас" />
-        <NavItem page="/assortment" title="Асортимент" />
+        <Assortment />
         <NavItem page="/delivery-and-payment" title="Доставка та оплата" />
         <NavItem page="/contacts" title="Контакти" />
         <Item>
