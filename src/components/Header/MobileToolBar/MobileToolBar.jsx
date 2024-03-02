ffb54acd3a2c2e0ea39700a2f36a0b40ad211885
1evanter <rita.oleksenkoo@gmail.com>
2024-03-03 00:39:42 +0200

    move FavoriteIcon

--- src/components/Header/MobileToolBar/MobileToolBar.jsx
+++ src/components/Header/MobileToolBar/MobileToolBar.jsx
@@ -1,16 +1,11 @@
 import { Wrapper, Button } from './MobileToolBar.styled';
-// import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import FavoriteBorderSharpIcon from '@mui/icons-material/FavoriteBorderSharp';
 // import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import LoginIcon from '@mui/icons-material/Login';
 import { FeedBackButton } from 'components/Shared/FeedbackButton/FeedbackButton';
-import { Badge } from '@mui/material';
 import { CartIcon } from 'components/Shared/CartIcon';
-// import { useMediaQuery } from 'react-responsive';
+import { FavoriteIcon } from 'components/Shared/FavoriteIcon';
 
 export const MobileToolBar = () => {
-  // const mobileVersion = useMediaQuery({ query: '(max-width:1279px)' });
-
   return (
     <Wrapper>
       <FeedBackButton />
@@ -18,38 +13,7 @@ export const MobileToolBar = () => {
         <CartIcon />
       </Button>
       <Button type="button">
-        <Badge
-          badgeContent={1}
-          color="secondary"
-          sx={{
-            '& .MuiBadge-badge': {
-              fontSize: 7,
-              fontWeight: '400',
-              color: 'background.default',
-              height: {
-                mobile: 12,
-                desktop: 15,
-              },
-              minWidth: {
-                mobile: 12,
-                desktop: 15,
-              },
-              top: 5,
-              right: 2,
-              p: 0,
-            },
-          }}
-        >
-          <FavoriteBorderSharpIcon
-            sx={{
-              color: 'background.paper',
-              width: '20px',
-              ' &:hover': {
-                color: 'secondary.main',
-              },
-            }}
-          />
-        </Badge>
+        <FavoriteIcon />
       </Button>
       <Button type="button">
         <LoginIcon
