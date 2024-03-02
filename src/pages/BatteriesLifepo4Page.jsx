1437c08da5c91c44426a0a04619d21f43f8ac8b6
Va7ul <vas7ul@gmail.com>
2024-03-02 14:09:38 +0200

    Update BatteriesLifepo4Page.jsx

--- src/pages/BatteriesLifepo4Page.jsx
+++ src/pages/BatteriesLifepo4Page.jsx
@@ -1,7 +1,7 @@
 import { ProductsSection } from 'components/Shared/ProductsSection/ProductsSection';
 
 const TypeLifepo4Page = () => {
-  return <ProductsSection category={'Акумулятори Lifepo4'} />;
+  return <ProductsSection category={'Акумулятори LiFePo4'} />;
 };
 
 export default TypeLifepo4Page;
