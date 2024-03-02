2e8d9857b27dfaade61f7db18e8075e8f0c19672
1evanter <rita.oleksenkoo@gmail.com>
2024-03-03 00:57:54 +0200

    remove hover

--- src/components/Header/Navigation/Navigation.styled.js
+++ src/components/Header/Navigation/Navigation.styled.js
@@ -14,7 +14,6 @@ export const NavList = styled.ul`
 export const CartButton = styled.button`
   display: flex;
   align-items: center;
-  padding: 34px 0;
   background-color: transparent;
   border: none;
   font-size: 15px;
@@ -23,9 +22,5 @@ export const CartButton = styled.button`
 
   @media screen and (min-width: 1280px) {
     color: ${props => props.theme.colors.backgroundPaper};
-
-    &:hover {
-      color: ${props => props.theme.colors.secondary};
-    }
   }
 `;
