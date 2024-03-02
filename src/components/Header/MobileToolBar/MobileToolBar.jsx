659ddab5e56101558f210a4a3b146427b0194a85
1evanter <rita.oleksenkoo@gmail.com>
2024-03-03 00:10:16 +0200

    add Badge for Cart and Favorite icons

--- src/components/Header/MobileToolBar/MobileToolBar.jsx
+++ src/components/Header/MobileToolBar/MobileToolBar.jsx
@@ -4,32 +4,83 @@ import FavoriteBorderSharpIcon from '@mui/icons-material/FavoriteBorderSharp';
 // import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import LoginIcon from '@mui/icons-material/Login';
 import { FeedBackButton } from 'components/Shared/FeedbackButton/FeedbackButton';
+import { Badge } from '@mui/material';
+// import { useMediaQuery } from 'react-responsive';
 
 export const MobileToolBar = () => {
+  // const mobileVersion = useMediaQuery({ query: '(max-width:1279px)' });
+
   return (
     <Wrapper>
       <FeedBackButton />
       <Button type="button">
-        <ShoppingCartOutlinedIcon
+        <Badge
+          badgeContent={1}
+          color="secondary"
           sx={{
-            color: 'background.paper',
-            width: '20px',
-            ' &:hover': {
-              color: 'secondary.main',
+            '& .MuiBadge-badge': {
+              fontSize: 7,
+              fontWeight: '400',
+              color: 'background.default',
+              height: {
+                mobile: 12,
+                desktop: 15,
+              },
+              minWidth: {
+                mobile: 12,
+                desktop: 15,
+              },
+              top: 5,
+              right: 2,
+              p: 0,
             },
           }}
-        />
+        >
+          <ShoppingCartOutlinedIcon
+            sx={{
+              p: 0,
+              color: 'background.paper',
+              width: '20px',
+              ' &:hover': {
+                color: 'secondary.main',
+              },
+            }}
+          />
+        </Badge>
       </Button>
       <Button type="button">
-        <FavoriteBorderSharpIcon
+        <Badge
+          badgeContent={1}
+          color="secondary"
           sx={{
-            color: 'background.paper',
-            width: '20px',
-            ' &:hover': {
-              color: 'secondary.main',
+            '& .MuiBadge-badge': {
+              fontSize: 7,
+              fontWeight: '400',
+              color: 'background.default',
+              height: {
+                mobile: 12,
+                desktop: 15,
+              },
+              minWidth: {
+                mobile: 12,
+                desktop: 15,
+              },
+              top: 5,
+              right: 2,
+              p: 0,
             },
           }}
-        />
+        >
+          <FavoriteBorderSharpIcon
+            sx={{
+              color: 'background.paper',
+              width: '20px',
+              ' &:hover': {
+                color: 'secondary.main',
+              },
+            }}
+          />
+        </Badge>
       </Button>
       <Button type="button">
         <LoginIcon
