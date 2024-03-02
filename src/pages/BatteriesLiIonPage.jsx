f0adf86add45fc2b1328c58d2f912ab188d9a225
Va7ul <vas7ul@gmail.com>
2024-03-02 14:09:35 +0200

    Update BatteriesLiIonPage.jsx

--- src/pages/BatteriesLiIonPage.jsx
+++ src/pages/BatteriesLiIonPage.jsx
@@ -1,7 +1,7 @@
 import { ProductsSection } from 'components/Shared/ProductsSection/ProductsSection';
 
 const TypeLiIonPage = () => {
-  return <ProductsSection category={'Акумулятори Li-po'} />;
+  return <ProductsSection category={'Акумулятори Li-Ion'} />;
 };
 
 export default TypeLiIonPage;
