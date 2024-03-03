33abeb186ef3e393cd04cb8e4700d42db6f215f9
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-03-03 17:21:30 +0200

    fix_useState

--- src/components/CardPage/Card.jsx
+++ src/components/CardPage/Card.jsx
@@ -1,5 +1,5 @@
 import { FaMinus, FaPlus } from 'react-icons/fa6';
-import { useState } from 'react';
+// import { useState } from 'react';
 // import Box from '@mui/material/Box';
 // import InputLabel from '@mui/material/InputLabel';
 // import MenuItem from '@mui/material/MenuItem';
