d1900459f8c3a6c4900c2cd7d73f0e82f80c76e0
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-03-04 00:36:25 +0200

    added_selectors

---
+++ src/redux/products/productsSelectors.js
@@ -0,0 +1,3 @@
+export const selectIsLoading = state => state.products.isLoading;
+export const selectError = state => state.products.error;
+export const selectOneProduct = state => state.products.oneProduct.result;
