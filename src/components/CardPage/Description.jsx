cad68c26d44051fa329483bc6f16aaea771dc183
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-03-06 10:56:07 +0200

    added_logic

--- src/components/CardPage/Description.jsx
+++ src/components/CardPage/Description.jsx
@@ -7,29 +7,30 @@ import {
   PriceBox,
   Price,
   Like,
-  Desc
+  Desc,
+  Item
   } from "./Card.styled";
 
 export const Description = () => {
-    const { codeOfGood, description, price, quantity} = useSelector(selectOneProduct);
+    const { codeOfGood, description, price, quantity } = useSelector(selectOneProduct);
 
-    // const newDescription = description.split(';');
+    const newDescription = description.split(';');
     
     return (
         <DescBox>
-             <TextBox>
-                    {quantity > 0 ? (
-                        <Text style={{color: 'rgba(20, 211, 16, 1)' }}>В наявності</Text>
-                    ) : (
-                        <Text style={{color: 'rgba(255, 0, 0, 1)' }}>Немає в наявності</Text>
-                    )}
-                    <Text>Код товару: {codeOfGood}</Text>
-                </TextBox>
-                <PriceBox>
-                    <Price>{price} грн</Price>
-                    <Like>Додати до списку бажань</Like>
-                </PriceBox>
-            {/* <Desc>{newDescription.map(i => <li key={i}>{i}</li>)}</Desc> */}
-       </DescBox>
+            <TextBox>
+                {quantity > 0 ? (
+                    <Text style={{ color: 'rgba(20, 211, 16, 1)' }}>В наявності</Text>
+                ) : (
+                    <Text style={{ color: 'rgba(255, 0, 0, 1)' }}>Немає в наявності</Text>
+                )}
+                <Text>Код товару: {codeOfGood}</Text>
+            </TextBox>
+            <PriceBox>
+                <Price>{price} грн</Price>
+                <Like>Додати до списку бажань</Like>
+            </PriceBox>
+            <Desc>{newDescription.map(i => <Item key={i}>{i}</Item>)}</Desc>
+        </DescBox>
     );
 };
