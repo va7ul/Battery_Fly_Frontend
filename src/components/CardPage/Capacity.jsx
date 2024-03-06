77d9e9b3bb52d97c066275027d0e260e4cc34930
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-03-06 15:57:41 +0200

    fix

--- src/components/CardPage/Capacity.jsx
+++ src/components/CardPage/Capacity.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectOneProduct } from '../../redux/products/productsSelectors';
 import { setPrice } from '../../redux/products/oneProductSlice';
-import { CapacityBox, Subtitle, Desc } from "./Card.styled";
+import { CapacityBox, Subtitle, SelectDiv, OptionsItem, Desc } from "./Card.styled";
 
 export const Capacity = () => {
     
@@ -24,10 +24,10 @@ export const Capacity = () => {
         <CapacityBox>
             <Subtitle>Ємність енергії:</Subtitle>
 
-            <select onChange={handleSelect} name="capacity">
-                <option>Виберіть опцію</option>
-                {capacityKeys.map(item => <option key={item}>{item}</option>)}
-            </select>
+            <SelectDiv onChange={handleSelect} name="capacity">
+                <OptionsItem>Виберіть опцію</OptionsItem>
+                {capacityKeys.map(item => <OptionsItem key={item}>{item}</OptionsItem>)}
+            </SelectDiv>
     
             <Desc>{newDescr.map(item => <li key={item}>{item}</li>)}</Desc>
         </CapacityBox>
