e2e4ddd1daeb72f317b8e71f083475781b9890a9
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-03-06 10:54:57 +0200

    fix_desktop_version

--- src/components/CardPage/Card.jsx
+++ src/components/CardPage/Card.jsx
@@ -1,35 +1,37 @@
-import { useEffect } from 'react';
 import { useMediaQuery } from 'react-responsive';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectOneProduct } from '../../redux/products/productsSelectors';
 import { getOneProduct } from '../../redux/products/productsOperations';
-import { CheckBox } from './Checkbox';
-import { Capacity } from './Capacity';
-import { Information } from './Information';
-import { Order } from './Order';
-import {
-    Wrapper,
-    Box,
-  Title,
-  Image
-} from './Card.styled';
+import { ProductPhoto } from './ProductPhoto';
 import { Description } from './Description';
+import { Capacity } from './Capacity';
+import { CheckBox } from './Checkbox';
+import { Order } from './Order';
+import { Information } from './Information';
+import {
+  Wrapper,
+  Box,
+  Title,
+  Case
+} from './Card.styled';
 
 export const Card = () => {
     const mobileVersion = useMediaQuery({ query: '(max-width:1279px)' });
     const dispatch = useDispatch();
     
-    const { name, image, information } = useSelector(selectOneProduct);
+    const { name, information } = useSelector(selectOneProduct);
 
     useEffect(() => {
-        dispatch(getOneProduct('0099'));
+        dispatch(getOneProduct('9'));
     }, [dispatch]);
 
     return (
         mobileVersion ? (<Wrapper>
+
             <Title>{name}</Title>
-            <Image src={image} alt={name} />
-               
+
+            <ProductPhoto />
             <Description />
             <Capacity />
             <CheckBox />
@@ -38,16 +40,24 @@ export const Card = () => {
             {information ? (<Information information={information} />) : (undefined)}
              
         </Wrapper>
+            
         ) : (
-            <Wrapper>
-                <Title>{name}</Title>
-                <Box>
-                    <Image src={image} alt={name} />
-                    <Description />
-                </Box>
-                <Capacity />
-                <CheckBox />
-                <Order />
+                
+            <Wrapper>  
+                <Case>
+                    <div>
+                        <ProductPhoto />
+                        <Capacity />
+                    </div>
+                    
+                    <Box>
+                        <Title>{name}</Title>
+                        <Description />
+                        <CheckBox />
+                        <Order />
+                    </Box>
+                </Case>
+                    
                 {information ? (<Information information={information} />) : (undefined)}
                 
             </Wrapper>
