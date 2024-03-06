4b4813fd54c404fa50f7de82394d2387cb7367c2
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-03-06 10:56:49 +0200

    created_ProductPhoto_component

---
+++ src/components/CardPage/ProductPhoto.jsx
@@ -0,0 +1,11 @@
+import { useSelector } from 'react-redux';
+import { selectOneProduct } from '../../redux/products/productsSelectors';
+import { Image } from "./Card.styled";
+
+export const ProductPhoto = () => {
+
+        const { name, image } = useSelector(selectOneProduct);
+    return (
+        <Image src={image} alt={name} />
+    );
+};
