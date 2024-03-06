d3bf653454ce56542ac35da5bc34efd072652b66
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-03-06 16:11:26 +0200

    fix

--- src/components/CardPage/ProductPhoto.jsx
+++ src/components/CardPage/ProductPhoto.jsx
@@ -4,7 +4,7 @@ import { Image } from "./Card.styled";
 
 export const ProductPhoto = () => {
 
-        const { name, image } = useSelector(selectOneProduct);
+    const { name, image } = useSelector(selectOneProduct);
     return (
         <Image src={image} alt={name} />
     );
