8d7f7bfed52b6d98bab3bdad47e38989c3418e0f
1evanter <rita.oleksenkoo@gmail.com>
2024-03-07 19:14:29 +0200

    create DesktopAssortment Menu

--- src/components/Header/Navigation/Assortment/DesktopAssortment/DesktopAssortment.jsx
+++ src/components/Header/Navigation/Assortment/DesktopAssortment/DesktopAssortment.jsx
@@ -1,5 +1,55 @@
 import { StyledLink } from '../../NavItem/NavItem.styled';
+import sprite from '../../../../../assets/images/sprite.svg';
+import { Wrapper, ArrowButton } from './DesktopAssortment.styled';
+import { useState } from 'react';
+import { Menu } from '@mui/material';
+import { AssortmentList } from '../AssortmentList/AssortmentList';
 
 export const DesktopAssortment = () => {
-  return <StyledLink to="/assortment">Асортимент</StyledLink>;
+  const [anchorEl, setAnchorEl] = useState();
+
+  const handleClick = event => {
+    if (anchorEl !== event.currentTarget) {
+      setAnchorEl(event.currentTarget);
+    }
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Wrapper>
+      <StyledLink to="/assortment">Асортимент</StyledLink>
+      <ArrowButton
+        type="button"
+        aria-owns={anchorEl ? 'simple-menu' : undefined}
+        aria-haspopup="true"
+        onClick={handleClick}
+        onMouseOver={handleClick}
+      >
+        <svg>
+          <use href={`${sprite}#arrow-left`}></use>
+        </svg>
+      </ArrowButton>
+
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        MenuListProps={{ onMouseLeave: handleClose }}
+        sx={{
+          '& .MuiMenu-paper': {
+            bgcolor: 'background.default',
+            width: '414px',
+            pl: '24px',
+            pr: '72px',
+          },
+        }}
+      >
+        <AssortmentList onClick={handleClose} />
+      </Menu>
+    </Wrapper>
+  );
 };
