325113242182e072ca34ca4569eb850549ea5477
1evanter <rita.oleksenkoo@gmail.com>
2024-03-08 00:39:51 +0200

    change fill color

--- src/components/Shared/HopeIconMobile/HopeIconMobile.styled.js
+++ src/components/Shared/HopeIconMobile/HopeIconMobile.styled.js
@@ -4,5 +4,5 @@ export const Icon = styled.svg`
   width: 14px;
   height: 14px;
   margin-right: 27px;
-  fill: ${props => props.theme.colors.svgColor.main};
+  fill: ${props => props.theme.colors.backgroundPaper};
 `;
