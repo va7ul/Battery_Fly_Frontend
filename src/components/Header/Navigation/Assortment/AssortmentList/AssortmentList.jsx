01ad87047ac40fd782537b417d72f1117e6dbb38
1evanter <rita.oleksenkoo@gmail.com>
2024-03-07 19:14:55 +0200

    Create AssortmentList.jsx

---
+++ src/components/Header/Navigation/Assortment/AssortmentList/AssortmentList.jsx
@@ -0,0 +1,19 @@
+import { ListItem } from './ListItem/ListItem';
+
+export const AssortmentList = () => {
+  return (
+    <ul>
+      <ListItem page="/sales" title="Акції" />
+      <ListItem page="/batteries" title="Акумулятори" />
+      <ListItem page="/assembly" title="Готові батареї" />
+      <ListItem page="/batteries-for-fpv" title="Батареї для FPV-дронів" />
+      <ListItem
+        page="/batteries-for-transport"
+        title="Батареї для електротранспорту"
+      />
+      <ListItem page="/batteries-for-toys" title="Батареї для іграшок" />
+      <ListItem page="/devices" title="Прилади" />
+      <ListItem page="/materials" title="Розхідні матеріали" />
+    </ul>
+  );
+};
