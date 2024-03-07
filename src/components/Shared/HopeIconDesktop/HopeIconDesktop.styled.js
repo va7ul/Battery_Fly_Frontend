5d9082723b2c87100811c07de75ecc37297f76a1
1evanter <rita.oleksenkoo@gmail.com>
2024-03-08 00:53:18 +0200

    Update HopeIconDesktop.styled.js

--- src/components/Shared/HopeIconDesktop/HopeIconDesktop.styled.js
+++ src/components/Shared/HopeIconDesktop/HopeIconDesktop.styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Icon = styled.svg`
   width: 27px;
+  height: 23px;
   margin-right: 37px;
   fill: ${props => props.theme.colors.secondary};
 `;
