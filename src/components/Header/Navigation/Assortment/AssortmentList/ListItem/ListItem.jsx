d9021a54ad20761001e512be93b0cf0fcff932eb
1evanter <rita.oleksenkoo@gmail.com>
2024-03-08 00:08:13 +0200

    ListItem styles

--- src/components/Header/Navigation/Assortment/AssortmentList/ListItem/ListItem.jsx
+++ src/components/Header/Navigation/Assortment/AssortmentList/ListItem/ListItem.jsx
@@ -1,10 +1,9 @@
-import { NavLink } from 'react-router-dom';
-import { Item } from './ListItem.styled';
+import { Item, StyledLink } from './ListItem.styled';
 
 export const ListItem = ({ page, title }) => {
   return (
     <Item>
-      <NavLink to={page}>{title}</NavLink>
+      <StyledLink to={page}>{title}</StyledLink>
     </Item>
   );
 };
--- src/components/Header/Navigation/Assortment/AssortmentList/ListItem/ListItem.styled.js
+++ src/components/Header/Navigation/Assortment/AssortmentList/ListItem/ListItem.styled.js
@@ -1,6 +1,23 @@
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Item = styled.li`
+  font-weight: 500;
   font-size: 15px;
   padding: 18px 0;
+  color: ${props => props.theme.colors.textPrimary};
+
+  @media screen and (min-width: 1280px) {
+    font-weight: 400;
+  }
+`;
+
+export const StyledLink = styled(NavLink)`
+  &.active {
+    color: ${props => props.theme.colors.secondary};
+  }
+
+  &:hover {
+    color: ${props => props.theme.colors.secondary};
+  }
 `;
