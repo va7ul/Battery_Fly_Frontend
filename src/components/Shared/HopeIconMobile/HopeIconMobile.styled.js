6c3f8ed9f41c4dead2ce45fcc611fda5af807040
1evanter <rita.oleksenkoo@gmail.com>
2024-03-08 00:29:48 +0200

    move HopeIcon-1 into Shared folder

---
+++ src/components/Shared/HopeIconMobile/HopeIconMobile.styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Icon = styled.svg`
+  width: 14px;
+  height: 14px;
+  margin-right: 27px;
+  fill: ${props => props.theme.colors.svgColor.main};
+`;
