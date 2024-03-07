73735b8d9ef72351269f45dfbc8228d30928273f
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-03-07 10:39:43 +0200

    added_icons

--- src/components/CardPage/ProductPhoto.jsx
+++ src/components/CardPage/ProductPhoto.jsx
@@ -1,11 +1,20 @@
 import { useSelector } from 'react-redux';
 import { selectOneProduct } from '../../redux/products/productsSelectors';
-import { Image } from "./Card.styled";
+import sprite from '../../assets/images/sprite.svg';
+import { PhotoBox, Image, IconSquare, IconSpiral } from "./Card.styled";
 
 export const ProductPhoto = () => {
 
     const { name, image } = useSelector(selectOneProduct);
     return (
-        <Image src={image} alt={name} />
+        <PhotoBox>
+            <Image src={image} alt={name} />
+            <IconSquare>
+                <use href={`${sprite}#icon-square`}></use>
+            </IconSquare>
+            <IconSpiral>
+                <use href={`${sprite}#icon-Spiral_2`}></use>
+            </IconSpiral>
+        </PhotoBox>
     );
 };
