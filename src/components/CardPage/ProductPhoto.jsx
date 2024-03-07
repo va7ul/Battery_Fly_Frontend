ad15f20ecb1f7d5cbe312fd6ae991e8382f6cceb
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-03-07 15:54:09 +0200

    fix_position_icon

--- src/components/CardPage/ProductPhoto.jsx
+++ src/components/CardPage/ProductPhoto.jsx
@@ -1,20 +1,12 @@
 import { useSelector } from 'react-redux';
 import { selectOneProduct } from '../../redux/products/productsSelectors';
-import sprite from '../../assets/images/sprite.svg';
-import { PhotoBox, Image, IconSquare, IconSpiral } from "./Card.styled";
+import { Image } from "./Card.styled";
 
 export const ProductPhoto = () => {
 
     const { name, image } = useSelector(selectOneProduct);
+
     return (
-        <PhotoBox>
-            <Image src={image} alt={name} />
-            <IconSquare>
-                <use href={`${sprite}#icon-square`}></use>
-            </IconSquare>
-            <IconSpiral>
-                <use href={`${sprite}#icon-Spiral_2`}></use>
-            </IconSpiral>
-        </PhotoBox>
+        <Image src={image} alt={name} />
     );
 };
