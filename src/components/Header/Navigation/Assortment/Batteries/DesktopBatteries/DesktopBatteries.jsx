953abaa008fc750538efdce10d95521c3299cc43
1evanter <rita.oleksenkoo@gmail.com>
2024-03-08 03:49:45 +0200

    add DesktopBatteries menu

--- src/components/Header/Navigation/Assortment/Batteries/DesktopBatteries/DesktopBatteries.jsx
+++ src/components/Header/Navigation/Assortment/Batteries/DesktopBatteries/DesktopBatteries.jsx
@@ -1,5 +1,59 @@
+import { useState } from 'react';
+import sprite from '../../../../../../assets/images/sprite.svg';
 import { StyledLink } from '../../../NavItem/NavItem.styled';
+import { Menu } from '@mui/material';
+import { BatteriesList } from '../BatteriesList/BatteriesList';
+import {
+  Wrapper,
+  ArrowButton,
+} from '../../DesktopAssortment/DesktopAssortment.styled';
 
 export const DesktopBatteries = () => {
-  return <StyledLink to="/batteries">Акумулятори</StyledLink>;
+  const [anchorEl, setAnchorEl] = useState();
+
+  const handleClick = event => {
+    if (anchorEl !== event.currentTarget) {
+      setAnchorEl(event.currentTarget);
+    }
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Wrapper>
+      <StyledLink to="/batteries">Акумулятори</StyledLink>
+      <ArrowButton
+        type="button"
+        aria-owns={anchorEl ? 'simple-menu' : undefined}
+        aria-haspopup="true"
+        onClick={handleClick}
+        onMouseOver={handleClick}
+      >
+        <svg>
+          <use href={`${sprite}#arrow-left`}></use>
+        </svg>
+      </ArrowButton>
+
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        MenuListProps={{ onMouseLeave: handleClose }}
+        sx={{
+          '& .MuiMenu-paper': {
+            bgcolor: 'background.default',
+            width: '185px',
+            pl: '20px',
+            pr: '20px',
+            borderRadius: '18px',
+          },
+        }}
+      >
+        <BatteriesList onClick={handleClose} />
+      </Menu>
+    </Wrapper>
+  );
 };
