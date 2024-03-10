455030afe25519cfd016e2d399ed3ed0b82ab308
1evanter <rita.oleksenkoo@gmail.com>
2024-03-10 21:25:12 +0200

    Update HopeIconMobile.styled.js

--- src/components/Shared/HopeIconMobile/HopeIconMobile.styled.js
+++ src/components/Shared/HopeIconMobile/HopeIconMobile.styled.js
@@ -4,6 +4,7 @@ export const Icon = styled.svg`
   width: 14px;
   height: 14px;
   margin-right: 27px;
+  margin-left: 15px;
   z-index: 2;
   fill: ${props => props.theme.colors.backgroundPaper};
 `;
