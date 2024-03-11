6b06414eba79fc01bc12c0f23d8958705a977223
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-03-12 00:24:49 +0200

    moved_selectStyles_to_styles

--- src/components/CardPage/Card.styled.js
+++ src/components/CardPage/Card.styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+// import Select from 'react-select';
 import { FaRegHeart, FaHeartCircleCheck } from 'react-icons/fa6';
 import { gradientTransitionBtn } from 'styles/GlobalStyled';
 
@@ -207,24 +208,6 @@ export const Item = styled.li`
   list-style: outside;
 `;
 
-// Capacity
-
-export const CapacityBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-`;
-
-export const Subtitle = styled.p`
-  font-size: 10px;
-  font-weight: 500;
-
-  @media screen and (min-width: 1280px) {
-    font-size: 20px;
-    font-weight: 600;
-  }
-`;
-
 // CheckBox
 
 export const Container = styled.div`
@@ -391,3 +374,105 @@ export const Info = styled.div`
     font-size: 20px;
   }
 `;
+
+// Capacity
+
+export const CapacityBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const Subtitle = styled.p`
+  font-size: 10px;
+  font-weight: 500;
+
+  @media screen and (min-width: 1280px) {
+    font-size: 20px;
+    font-weight: 600;
+  }
+`;
+
+export const selectStyles = {
+  control: styles => ({
+    ...styles,
+    backgroundColor: 'rgba(225, 225, 225, 1)',
+    borderRadius: '6px',
+    borderStyle: 'none',
+    width: '106px',
+    maxHeight: '30px',
+    minHeight: '25px',
+    border: '1px solid rgb(31, 31, 31)',
+    ':hover': { borderColor: 'currentColor', boxShadow: 'none' },
+    '@media only screen and (min-width: 1280px)': {
+      width: '200px',
+      minHeight: '42px',
+      borderRadius: '12px',
+    },
+  }),
+  menu: styles => ({
+    ...styles,
+    backgroundColor: 'rgba(225, 225, 225, 1)',
+    fontSize: '10px',
+    width: '106px',
+    border: '1px solid rgb(31, 31, 31)',
+    '@media only screen and (min-width: 1280px)': {
+      fontSize: '20px',
+      width: '200px',
+      borderRadius: '12px',
+    },
+  }),
+  input: styles => ({ ...styles, display: 'none' }),
+  option: (styles, { isDisabled, isFocused, isSelected }) => {
+    return {
+      ...styles,
+      backgroundColor: isFocused ? 'rgb(255, 208, 100)' : undefined,
+      color: !isDisabled
+        ? isSelected
+          ? 'rgb(31, 31, 31);'
+          : 'rgb(31, 31, 31);'
+        : undefined,
+      cursor: isDisabled ? 'not-allowed' : 'default',
+      padding: '2px 8px',
+      ':active': {
+        ...styles[':active'],
+        backgroundColor: !isDisabled
+          ? isSelected
+            ? 'rgb(255, 208, 100)'
+            : 'rgb(255, 208, 100)'
+          : undefined,
+      },
+      '@media only screen and (min-width: 1280px)': {
+        borderRadius: '6px',
+      },
+    };
+  },
+  placeholder: styles => ({
+    ...styles,
+    fontSize: '10px',
+    padding: '0px 8px',
+    margin: 0,
+    '@media only screen and (min-width: 1280px)': {
+      fontSize: '20px',
+    },
+  }),
+  indicatorsContainer: styles => ({
+    ...styles,
+    height: '30px',
+    width: '38px',
+    '@media only screen and (min-width: 1280px)': {
+      height: '42px',
+      width: '40px',
+    },
+  }),
+  valueContainer: styles => ({ ...styles, padding: 0 }),
+  singleValue: styles => ({
+    ...styles,
+    fontSize: '10px',
+    margin: 0,
+    padding: '8px',
+    '@media only screen and (min-width: 1280px)': {
+      fontSize: '20px',
+    },
+  }),
+};
