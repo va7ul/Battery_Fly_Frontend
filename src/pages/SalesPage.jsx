35902eb3d18fc608dc983d820bda1bc99984e057
Va7ul <vas7ul@gmail.com>
2024-03-11 16:10:11 +0200

    add dispatch on the page

--- src/pages/SalesPage.jsx
+++ src/pages/SalesPage.jsx
@@ -1,6 +1,15 @@
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getSales } from '../redux/products/productsOperations';
 import { ProductsSection } from 'components/Shared/ProductsSection/ProductsSection';
 
 const SalesPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getSales());
+  }, [dispatch]);
+
   return <ProductsSection category={'Акції'} />;
 };
 
