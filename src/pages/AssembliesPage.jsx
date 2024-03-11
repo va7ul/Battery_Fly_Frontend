41ea71e73e4a5510528323cd0f81befdb61b0627
Va7ul <vas7ul@gmail.com>
2024-03-11 16:11:04 +0200

    add dispatch on the page

--- src/pages/AssembliesPage.jsx
+++ src/pages/AssembliesPage.jsx
@@ -1,6 +1,15 @@
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getAssemblies } from '../redux/products/productsOperations';
 import { ProductsSection } from 'components/Shared/ProductsSection/ProductsSection';
 
 const AssembliesPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAssemblies());
+  }, [dispatch]);
+
   return <ProductsSection category={'Готові батареї'} />;
 };
 
