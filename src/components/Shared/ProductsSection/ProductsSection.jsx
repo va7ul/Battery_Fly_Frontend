514b896246ea4c13ded238fe72c70189351b79a1
Va7ul <vas7ul@gmail.com>
2024-03-11 16:14:15 +0200

    change paddings & background

--- src/components/Shared/ProductsSection/ProductsSection.jsx
+++ src/components/Shared/ProductsSection/ProductsSection.jsx
@@ -4,7 +4,7 @@ import { StyledContainer, StyledTitle } from './ProductsSection.styled';
 export const ProductsSection = ({ category }) => {
   return (
     <section>
-      <StyledContainer>
+      <StyledContainer category={category}>
         <StyledTitle>{category}</StyledTitle>
         <ProductsList />
       </StyledContainer>
