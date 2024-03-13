85e2a98789cee58a12a94e71f1e6159b06d430b5
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-03-13 16:38:25 +0200

    update_ Slider

--- src/components/CardPage/ProductPhoto.jsx
+++ src/components/CardPage/ProductPhoto.jsx
@@ -1,9 +1,9 @@
-import { useSelector } from 'react-redux';
-import { selectOneProduct } from '../../redux/products/productsSelectors';
-import { Image } from "./Card.styled";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useSelector } from 'react-redux';
+import { selectOneProduct } from '../../redux/products/productsSelectors';
+import { SliderDiv, ImageSmall, Image } from "./Card.styled";
 
 export const ProductPhoto = () => {
 
@@ -13,36 +13,35 @@ export const ProductPhoto = () => {
     //     <Image src={image[0]} alt={name} />
     // );
 
-  const settings = {
-    customPaging: function(i) {
-      return (
-        <a href={image[i]}>
-          <img src={image[i]} alt={name} /> 
-        </a>
-      );
-    },
-    dots: true,
-    dotsClass: "slick-dots slick-thumb",
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-      slidesToScroll: 1,
-      // arrows: false,
-      focusOnSelect: true,
-      //   slide: 'img'
-      //   adaptiveHeight: true
-    //   centerMode: true
+    const settings = {
+        customPaging: function (i) {
+            return (
+                <a href={image[i]}>
+                    <ImageSmall src={image[i]} alt={name} />
+                </a>
+            );
+        },
+        dots: true,
+        dotsClass: "slick-dots slick-thumb",
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        // arrows: false,
+        focusOnSelect: true,
+        //   slide: 'li'
+        //   adaptiveHeight: true
+        //   centerMode: true
+    };
 
-   
-  };
-  return (
-    <div style={{width: '320px', padding: '0px 0px 30px 0px', margin: '0 auto'}} className="slider-container">
-      <Slider {...settings}>
-              {image.map((item) => <div key={item}>
-        <Image src={item} alt={name} /> 
-    </div>
-)}
-      </Slider>
-    </div>
-  );
+    return (
+        <SliderDiv className="slider-container">
+            <Slider {...settings}>
+                {image.map((item) => <div key={item}>
+                    <Image src={item} alt={name} />
+                </div>
+                )}
+            </Slider>
+        </SliderDiv>
+    );
 };
