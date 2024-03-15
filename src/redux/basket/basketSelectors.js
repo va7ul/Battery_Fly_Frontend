526346599179013082d486d98adc44d6a9fac590
1evanter <rita.oleksenkoo@gmail.com>
2024-03-15 21:01:04 +0200

    change selectTotalPrice to selectTotal

--- src/redux/basket/basketSelectors.js
+++ src/redux/basket/basketSelectors.js
@@ -1,4 +1,4 @@
 export const selectIsLoading = state => state.products.isLoading;
 export const selectError = state => state.products.error;
 export const selectItems = state => state.basket.items;
-export const selectTotalPrice = state => state.basket.totalPrice;
+export const selectTotal = state => state.basket.total;
