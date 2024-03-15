2a872a15c8ed68fc3aad3368c5058632b975b8a3
1evanter <rita.oleksenkoo@gmail.com>
2024-03-16 01:01:23 +0200

    Update CartModal.styled.js

--- src/components/Shared/CartModal/CartModal.styled.js
+++ src/components/Shared/CartModal/CartModal.styled.js
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 
 export const Title = styled.h3`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 0;
   font-size: 20px;
   font-weight: 600;
+  border-bottom: 1px solid ${props => props.theme.colors.greyBackgroundPaper};
 `;
