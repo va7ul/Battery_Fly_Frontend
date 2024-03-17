0bd5a9e83bad401461c5461347b9f360cf1641a6
Va7ul <vas7ul@gmail.com>
2024-03-17 17:41:16 +0200

    change salePrice to discount

--- src/utils/helpers/getNewPrice.js
+++ src/utils/helpers/getNewPrice.js
@@ -1,16 +1,13 @@
-export const getNewPrice = (salePrice, price, newPrice) => {
-  if (salePrice > 0) {
-    if (typeof price === 'string') {
-      const result = price.split('-');
-      const minPrice = Number(result[0]);
-      const maxPrice = Number(result[1]);
-      const minNewPrice = Math.ceil(minPrice - (minPrice * salePrice) / 100);
-      const maxNewPrice = Math.ceil(maxPrice - (maxPrice * salePrice) / 100);
-      newPrice = `${minNewPrice} - ${maxNewPrice}`;
-    } else {
-      newPrice = Math.ceil((price * salePrice) / 100 - price);
-    }
+export const getNewPrice = (discount, price, newPrice) => {
+  if (typeof price === 'string') {
+    const result = price.split('-');
+    const minPrice = Number(result[0]);
+    const maxPrice = Number(result[1]);
+    const minNewPrice = Math.ceil(minPrice - (minPrice * discount) / 100);
+    const maxNewPrice = Math.ceil(maxPrice - (maxPrice * discount) / 100);
+    newPrice = `${minNewPrice} - ${maxNewPrice}`;
+  } else {
+    newPrice = Math.ceil(price - (price * discount) / 100);
   }
-
   return newPrice;
 };
