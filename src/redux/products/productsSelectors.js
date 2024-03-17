0feeeb919bea48b0980f5ded83e058ce66646e9a
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-03-17 19:47:07 +0200

    added_ selectOneProductPrice

--- src/redux/products/productsSelectors.js
+++ src/redux/products/productsSelectors.js
@@ -3,6 +3,8 @@ import { createSelector } from '@reduxjs/toolkit';
 export const selectIsLoading = state => state.products.isLoading;
 export const selectError = state => state.products.error;
 export const selectOneProduct = state => state.products.oneProduct.result;
+export const selectOneProductPrice = state =>
+  state.products.oneProduct.result.priceOneProduct;
 export const selectProducts = state => state.products.productsList.allProducts;
 
 export const selectPopularProducts = createSelector(
