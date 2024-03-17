459a61b203040636e0812f02233e2f0fe1a11126
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-03-17 18:46:01 +0200

    added_logic_for_ quantityOrdered

--- src/components/CardPage/Order.jsx
+++ src/components/CardPage/Order.jsx
@@ -1,3 +1,7 @@
+import toast, { Toaster } from 'react-hot-toast';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectOneProduct } from '../../redux/products/productsSelectors';
 import { FaMinus, FaPlus } from 'react-icons/fa6';
 import {
   OrderBox,
@@ -9,16 +13,66 @@ import {
   OrderButton, } from "./Card.styled";
 
 export const Order = () => {
+
+    const [quantityOrdered, setQuantityOrdered] = useState(1);
+    const { quantity, price } = useSelector(selectOneProduct);
+    
+
+    const plusOne = () => {
+        if (quantityOrdered < quantity) {
+            setQuantityOrdered(state => Number(state) + 1);
+            
+        } else if (quantityOrdered >= quantity) {
+               toast.success(`Максимальна кількість: ${quantity}`, {
+                id: 'clipboard',
+                duration: 4000,
+            })
+        }
+    };
+
+    const minusOne = () => {
+        if (quantityOrdered > 1) {
+            setQuantityOrdered(state => Number(state) - 1);
+        }
+    };
+
+    const setValue = e => {
+        if (e.target.value > quantity) {
+            setQuantityOrdered(Number(quantity))
+            toast.success(`Максимальна кількість: ${quantity}`, {
+                id: 'clipboard',
+                duration: 4000,
+            })
+        }
+        if (e.target.value <= quantity) {
+            setQuantityOrdered(Number(e.target.value) || '');
+        }
+    };
+    
+    const minValue = () => {
+        if (quantityOrdered === '') {
+            setQuantityOrdered(1);
+        }
+    };
+
     return (
         <OrderBox>
             <CounterBox>
-                <Button>
+                <Button onClick={minusOne}>
                     <div>
                         <FaMinus />
                     </div>
                 </Button>
-                <Input type="number" min="1" placeholder="1 шт"></Input>
-                <Button>
+                <Input
+                    type="number"
+                    min="1"
+                    max={quantity}
+                    // placeholder="1 шт"
+                    onBlur={minValue}
+                    onChange={setValue}
+                    value={quantityOrdered}>
+                </Input>
+                <Button onClick={plusOne}>
                     <div>
                         <FaPlus />
                     </div>
@@ -32,6 +86,11 @@ export const Order = () => {
                     <div>Швидке замовлення</div>
                 </OrderButton>
             </ButtonBox>
+            <div><Toaster
+                position="top-center"
+                reverseOrder={false}
+            /></div>
+
         </OrderBox>
     );
 };
