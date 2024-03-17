a22dab0f17b167db190f3b9be7bee19cdb4d26e1
Va7ul <vas7ul@gmail.com>
2024-03-17 17:41:59 +0200

    delete +/- buttons

--- src/components/Shared/ProductsCard/ProductsCard.jsx
+++ src/components/Shared/ProductsCard/ProductsCard.jsx
@@ -1,6 +1,4 @@
 import { useDispatch, useSelector } from 'react-redux';
-// import {  useState } from 'react';
-// import { FaMinus, FaPlus } from 'react-icons/fa6';
 import noImage from '../../../assets/images/no-image-available.webp';
 import { addItem } from '../../../redux/basket/basketSlice';
 import {
@@ -12,22 +10,19 @@ import {
   PriceContainer,
   PriceNew,
   PriceOld,
-  // CounterWrapper,
-  // CounterBtn,
-  // CounterInput,
   ChooseBtn,
-  // GoToBasketBtn,
 } from './ProductsCard.styled';
 import { selectItems } from '../../../redux/basket/basketSelectors';
 import { getNewPrice } from 'utils/helpers/index';
 
 export const ProductsCard = ({ product }) => {
-  const { codeOfGood, image, name, price, salePrice, capacity } = product;
+  const { codeOfGood, image, name, price, discount, sale, capacity } = product;
   const dispatch = useDispatch();
-  // const [quantityOrdered, setQuantityOrdered] = useState(1);
 
   let newPrice = price;
-  newPrice = getNewPrice(salePrice, price, newPrice);
+  if (sale) {
+    newPrice = getNewPrice(discount, price, newPrice);
+  }
 
   const addDefaultImg = e => {
     e.currentTarget.src = `${noImage}`;
@@ -37,22 +32,6 @@ export const ProductsCard = ({ product }) => {
   const isInBasket = basketItems.find(
     basketItem => basketItem.codeOfGood === codeOfGood
   );
-
-  // const plusOne = () => {
-  //   setQuantityOrdered(state => state + 1);
-  // };
-
-  // const minusOne = () => {
-  //   if (quantityOrdered > 1) {
-  //     setQuantityOrdered(state => state - 1);
-  //   }
-  // };
-
-  // const setValue = e => {
-  //   if (Number(e.target.value) > 0) {
-  //     setQuantityOrdered(Number(e.target.value));
-  //   }
-  // };
 
   const addToBasket = e => {
     dispatch(
@@ -87,26 +66,8 @@ export const ProductsCard = ({ product }) => {
         </a>
         <PriceContainer>
           <PriceNew>{newPrice} грн</PriceNew>
-          <PriceOld>{price} грн</PriceOld>
+          {sale && <PriceOld>{price} грн</PriceOld>}
         </PriceContainer>
-        {/* <CounterWrapper>
-          <CounterBtn onClick={minusOne}>
-            <div>
-              <FaMinus />
-            </div>
-          </CounterBtn>
-          <CounterInput
-            onChange={setValue}
-            type="integer"
-            min="1"
-            value={quantityOrdered}
-          />
-          <CounterBtn onClick={plusOne}>
-            <div>
-              <FaPlus />
-            </div>
-          </CounterBtn>
-        </CounterWrapper> */}
         {!capacity ? (
           !isInBasket ? (
             <ChooseBtn onClick={addToBasket}>
