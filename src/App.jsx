14813d8ed0a43d7535ec188087de0eecd32780a6
Va7ul <vas7ul@gmail.com>
2024-03-18 16:08:03 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,32 @@ import { Layout } from './components/Layout/Layout';
 // import { selectIsRefreshing } from 'redux/auth/authSelectors.js';
 // import { refreshUser } from 'redux/auth/authOperations.js';
 
-const lazyLoadPage = importPath => lazy(() => import(`./src${importPath}`));
+const lazyLoadPage = importPath => lazy(() => import(`./pages/${importPath}`));
 
-const MainPage = lazyLoadPage('pages/MainPage.jsx');
-const AboutPage = lazyLoadPage('pages/AboutPage.jsx');
-const AssortmentPage = lazyLoadPage('pages/AssortmentPage.jsx');
-const SalesPage = lazyLoadPage('pages/SalesPage.jsx');
-const BatteriesPage = lazyLoadPage('pages/BatteriesPage.jsx');
-const Batteries18650Page = lazyLoadPage('pages/Batteries18650Page.jsx');
-const Batteries21700Page = lazyLoadPage('pages/Batteries21700Page.jsx');
-const Batteries32650Page = lazyLoadPage('pages/Batteries32650Page.jsx');
-const BatteriesLiPoPage = lazyLoadPage('pages/BatteriesLipoPage.jsx');
-const BatteriesLifepo4Page = lazyLoadPage('pages/BatteriesLifepo4Page.jsx');
-const AssemblyPage = lazyLoadPage('pages/AssembliesPage.jsx');
-const BatteriesForFPVPage = lazyLoadPage('pages/BatteriesForFPVPage.jsx');
-const BatteriesForTransportPage = lazyLoadPage(
-  'pages/BatteriesForTransportPage.jsx'
-);
-const BatteriesForToysPage = lazyLoadPage('pages/BatteriesForToysPage.jsx');
-const DevicesPage = lazyLoadPage('pages/DevicesPage.jsx');
-const MaterialsPage = lazyLoadPage('pages/MaterialsPage.jsx');
-const CardPage = lazyLoadPage('pages/CardPage.jsx');
-const DeliveryPaymentPage = lazyLoadPage('pages/DeliveryPaymentPage.jsx');
-const ContactsPage = lazyLoadPage('pages/ContactsPage.jsx');
-const GuaranteesPage = lazyLoadPage('pages/GuaranteesPage.jsx');
-const CheckoutPage = lazyLoadPage('pages/CheckoutPage.jsx');
-const UserProfilePage = lazyLoadPage('pages/UserProfilePage.jsx');
-const OrdersHistoryPage = lazyLoadPage('pages/OrdersHistoryPage.jsx');
-const FavoritesPage = lazyLoadPage('pages/FavoritesPage.jsx');
+const MainPage = lazyLoadPage('MainPage.jsx');
+const AboutPage = lazyLoadPage('AboutPage.jsx');
+const AssortmentPage = lazyLoadPage('AssortmentPage.jsx');
+const SalesPage = lazyLoadPage('SalesPage.jsx');
+const BatteriesPage = lazyLoadPage('BatteriesPage.jsx');
+const Batteries18650Page = lazyLoadPage('Batteries18650Page.jsx');
+const Batteries21700Page = lazyLoadPage('Batteries21700Page.jsx');
+const Batteries32650Page = lazyLoadPage('Batteries32650Page.jsx');
+const BatteriesLiPoPage = lazyLoadPage('BatteriesLipoPage.jsx');
+const BatteriesLifepo4Page = lazyLoadPage('BatteriesLifepo4Page.jsx');
+const AssemblyPage = lazyLoadPage('AssembliesPage.jsx');
+const BatteriesForFPVPage = lazyLoadPage('BatteriesForFPVPage.jsx');
+const BatteriesForTransportPage = lazyLoadPage('BatteriesForTransportPage.jsx');
+const BatteriesForToysPage = lazyLoadPage('BatteriesForToysPage.jsx');
+const DevicesPage = lazyLoadPage('DevicesPage.jsx');
+const MaterialsPage = lazyLoadPage('MaterialsPage.jsx');
+const CardPage = lazyLoadPage('CardPage.jsx');
+const DeliveryPaymentPage = lazyLoadPage('DeliveryPaymentPage.jsx');
+const ContactsPage = lazyLoadPage('ContactsPage.jsx');
+const GuaranteesPage = lazyLoadPage('GuaranteesPage.jsx');
+const CheckoutPage = lazyLoadPage('CheckoutPage.jsx');
+const UserProfilePage = lazyLoadPage('UserProfilePage.jsx');
+const OrdersHistoryPage = lazyLoadPage('OrdersHistoryPage.jsx');
+const FavoritesPage = lazyLoadPage('FavoritesPage.jsx');
 
 export const App = () => {
   // const dispatch = useDispatch();
