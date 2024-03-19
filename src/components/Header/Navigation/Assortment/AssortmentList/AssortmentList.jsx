c6f1af216863131445c391f41e20e0c9ed721c09
1evanter <rita.oleksenkoo@gmail.com>
2024-03-19 23:21:02 +0200

    clear comments

--- src/components/Header/Navigation/Assortment/AssortmentList/AssortmentList.jsx
+++ src/components/Header/Navigation/Assortment/AssortmentList/AssortmentList.jsx
@@ -7,43 +7,30 @@ import {
   setSubMenuOpen,
 } from '../../../../../redux/menu/menuSlice';
 
-export const AssortmentList = () =>
-  // {
-  //   handleClick,
-  //   openSubDrawer,
-  //   setOpenSubDrawer,
-  //   isOpen,
-  //   setIsOpen,
-  // }
-  {
-    const dispatch = useDispatch();
-    const isSubMenuOpen = useSelector(selectSubMenu);
+export const AssortmentList = () => {
+  const dispatch = useDispatch();
+  const isSubMenuOpen = useSelector(selectSubMenu);
 
-    const closeSubDrawer = () => {
-      if (isSubMenuOpen) {
-        dispatch(setSubMenuOpen(false));
-        dispatch(setMenuOpen(false));
-      }
-    };
-
-    return (
-      <ul onClick={closeSubDrawer}>
-        <ListItem page="/sales" title="Акції" />
-        <Batteries
-        // openSubDrawer={openSubDrawer}
-        // setOpenSubDrawer={setOpenSubDrawer}
-        // isOpen={isOpen}
-        // setIsOpen={setIsOpen}
-        />
-        <ListItem page="/assembly" title="Готові батареї" />
-        <ListItem page="/batteries-for-fpv" title="Батареї для FPV-дронів" />
-        <ListItem
-          page="/batteries-for-transport"
-          title="Батареї для електротранспорту"
-        />
-        <ListItem page="/batteries-for-toys" title="Батареї для іграшок" />
-        <ListItem page="/devices" title="Прилади" />
-        <ListItem page="/materials" title="Розхідні матеріали" />
-      </ul>
-    );
+  const closeSubDrawer = () => {
+    if (isSubMenuOpen) {
+      dispatch(setSubMenuOpen(false));
+      dispatch(setMenuOpen(false));
+    }
   };
+
+  return (
+    <ul onClick={closeSubDrawer}>
+      <ListItem page="/sales" title="Акції" />
+      <Batteries />
+      <ListItem page="/assembly" title="Готові батареї" />
+      <ListItem page="/batteries-for-fpv" title="Батареї для FPV-дронів" />
+      <ListItem
+        page="/batteries-for-transport"
+        title="Батареї для електротранспорту"
+      />
+      <ListItem page="/batteries-for-toys" title="Батареї для іграшок" />
+      <ListItem page="/devices" title="Прилади" />
+      <ListItem page="/materials" title="Розхідні матеріали" />
+    </ul>
+  );
+};
