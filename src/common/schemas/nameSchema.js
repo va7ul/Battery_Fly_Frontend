1999bf6469441196ee901b8a3219b1478c78c814
NatalyaMaterinskaya <natulka-91@ukr.net>
2024-03-20 13:20:39 +0200

    Added password to SighUpForm

--- src/common/schemas/nameSchema.js
+++ src/common/schemas/nameSchema.js
@@ -4,7 +4,7 @@ import { nameRegex } from '../regex';
 export const nameSchema = Yup.object({
   name: Yup.string()
     .min(2, "Введіть своє ім'я, будь ласка")
-    .max(70, "Введіть своє ім'я, будь ласка")
+    .max(30, "Введіть своє ім'я, будь ласка")
     .trim("Введіть своє ім'я, будь ласка")
     .matches(nameRegex, "Введіть своє ім'я, будь ласка")
     .required("Введіть своє ім'я, будь ласка"),
--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,5 +1,5 @@
 import { Formik } from 'formik';
-import { nameSchema } from '../../common/schemas/nameSchema';
+import { signUpSchema } from '../../common/schemas/signUpSchema';
 
 import {
   Btn,
@@ -8,44 +8,88 @@ import {
   StyledField,
   StyledForm,
 } from './SignUpForm.styled';
+import { IconButton, Input, InputAdornment, InputLabel } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { useState } from 'react';
 
 export const SignUpForm = () => {
-    return (
-      <Formik
-        initialValues={{
-          firstName: '',
-          lastName: '',
-          email: '',
-        }}
-        validationSchema={nameSchema}
-        onSubmit={(values, actions) => {
-          actions.resetForm();
-        }}
-      >
-        <StyledForm>
-          <Label>
-            Ім'я
-            <StyledField name="firstName" type="text" />
-            <StyledErrorMessage name="firstName" component="div" />
-          </Label>
+  const [showPassword, setShowPassword] = useState(false);
 
-          <Label>
-            Прізвище
-            <StyledField name="lastName" type="text" />
-            <StyledErrorMessage name="lastName" component="div" />
-          </Label>
+  const handleClickShowPassword = () => setShowPassword(show => !show);
+  return (
+    <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+      }}
+      validationSchema={signUpSchema}
+      onSubmit={(values, actions) => {
+        actions.resetForm();
+      }}
+    >
+      <StyledForm>
+        <Label>
+          Ім'я
+          <StyledField name="firstName" type="text" />
+          <StyledErrorMessage name="firstName" component="div" />
+        </Label>
 
-          <Label>
-            E-пошта
-            <StyledField name="email" type="email" />
-            <StyledErrorMessage name="email" component="div" />
-          </Label>
-          <Btn type="submit">
-            <div>Зареєструватись</div>
-          </Btn>
-        </StyledForm>
-      </Formik>
-    );
+        <Label>
+          Прізвище
+          <StyledField name="lastName" type="text" />
+          <StyledErrorMessage name="lastName" component="div" />
+        </Label>
+
+        <Label>
+          E-пошта
+          <StyledField name="email" type="email" />
+          <StyledErrorMessage name="email" component="div" />
+        </Label>
+
+        <InputLabel
+          sx={{
+            fontSize: '10px',
+            fontWeight: '600',
+            color: 'rgba(31, 31, 31, 1);',
+          }}
+          htmlFor="standard-adornment-password"
+        >
+          Пароль
+        </InputLabel>
+        <Input
+          id="standard-adornment-password"
+          name="password"
+          type={showPassword ? 'text' : 'password'}
+          sx={{
+            width: '261px',
+            height: '28px',
+            padding: '4px 8px',
+            // border: '1px solid rgb(31, 31, 31)',
+            // borderRadius: '6px',
+            fontSize: '10px',
+
+            // '& .MuiInputBase-root.MuiInput-root:before': {
+            //   borderBottom: '0px solid rgba(0, 0, 0, 0.42)',
+            // },
+          }}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+
+        <Btn type="submit">
+          <div>Зареєструватись</div>
+        </Btn>
+      </StyledForm>
+    </Formik>
+  );
 };
-
-
