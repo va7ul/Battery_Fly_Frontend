8ab4f9524516c7c5a1ccb9f7c7a74ac2f8861260
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-03-20 20:54:42 +0200

    changed_logic

--- src/components/CardPage/Checkbox.jsx
+++ src/components/CardPage/Checkbox.jsx
@@ -1,19 +1,15 @@
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-// import FormHelperText from '@mui/material/FormHelperText';
 import { styled } from '@mui/material/styles';
-// import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectOneProduct, selectSelectedSealing, selectSelectedHolder, selectOneProductPrice, selectQuantityOrders, selectSealingPrice, selectHolderPrice } from '../../redux/products/productsSelectors';
-import { setPrice, setSelectedHolder, setSelectedSealing } from '../../redux/products/oneProductSlice';
+import { setSelectedHolder, setSelectedSealing } from '../../redux/products/oneProductSlice';
 import { themeMUI } from '../../styles/GlobalStyled';
 import { yellow } from '@mui/material/colors';
-import { Subtitle, Container } from './Card.styled';
+import { Subtitle, Container, Group, ExtraPrice } from './Card.styled';
 
 const StyledFormGroup = styled(FormGroup)({
-    // flexDirection: 'row',
-    // flexWrap: 'nowrap',
     gap: '30px',
     '& .MuiButtonBase-root': {
         padding: '0px 10px 0px 0px'
@@ -43,136 +39,57 @@ const StyledCheckbox = styled(Checkbox)({
 export const CheckBox = () => {
     const dispatch = useDispatch();
 
-     const { price, capacity, capacityKey, holder} = useSelector(selectOneProduct);
+    const { holder } = useSelector(selectOneProduct);
     const selectedSealing = useSelector(selectSelectedSealing);
     const selectedHolder = useSelector(selectSelectedHolder);
     const oneProductPrice = useSelector(selectOneProductPrice);
-    const quantityOrders = useSelector(selectQuantityOrders);
     const sealingPrice = useSelector(selectSealingPrice);
     const holderPrice = useSelector(selectHolderPrice);
 
+    const handleSealing = (e) => {
+        dispatch(setSelectedSealing(e.target.checked));
+    };
 
-// useEffect(() => {
-//     if (typeof oneProductPrice !== "string") {
-//         // Calculate the total price based on quantityOrders and oneProductPrice
-//         let totalPrice = quantityOrders * oneProductPrice;
-
-//         // If selectedSealing is true, add sealing price to total price
-//         if (selectedSealing) {
-//             totalPrice += sealingPrice;
-//         }
-
-//         // Dispatch actions to update state
-//         dispatch(setSealingPrice(100 * quantityOrders));
-//         dispatch(setPrice(totalPrice));
-//     }
-// }, [dispatch, quantityOrders, oneProductPrice, sealingPrice, selectedSealing]);
-
-
-
-
-    // const handleSealing = (e) => {
-    //     if (e.target.checked) {
-    //         dispatch(setSelectedSealing(true))
-    //         if (typeof price === 'number' && e.target.checked) {
-    //             dispatch(setPrice(oneProductPrice * quantityOrders + sealingPrice))
-
-    //         };
-    //         return;
-    //     };
-    //     dispatch(setSelectedSealing(false))
-    //     if (typeof price === 'number' && !e.target.checked) {
-            
-    //         dispatch(setPrice(price - sealingPrice));
-           
-    //     };
-    // };
-
-
-const handleSealing = (e) => {
-    const isChecked = e.target.checked;
-
-    dispatch(setSelectedSealing(isChecked));
-
-    if (typeof price === 'number') {
-        if (isChecked) {
-            dispatch(setPrice(oneProductPrice * quantityOrders + sealingPrice));
-        } else {
-            dispatch(setPrice(price - sealingPrice));
-        }
-    }
-};
-
-    
-
-    // const handleHolder = (e) => {
-    //     if (e.target.checked) {
-    //         dispatch(setSelectedHolder(true))
-    //         if (typeof price === 'number' && e.target.checked) {
-    //             dispatch(setPrice(price + (capacity[capacityKey].holder * 2)));
-    //         };
-    //         return
-    //     };
-    //     dispatch(setSelectedHolder(false))
-    //     if (typeof price === 'number' && !e.target.checked) {
-    //         dispatch(setPrice(price - (capacity[capacityKey].holder * 2)));
-    //     };
-    // };
 
     const handleHolder = (e) => {
-    const isChecked = e.target.checked;
+        dispatch(setSelectedHolder(e.target.checked));
 
-    dispatch(setSelectedHolder(isChecked));
-
-    if (typeof price === 'number') {
-        const holderCost = capacity[capacityKey]?.holder * 2 || 0;
-
-        if (isChecked) {
-            dispatch(setPrice(price + holderCost));
-        } else {
-            dispatch(setPrice(price - holderCost));
-        }
-    }
-};
+    };
 
 
     return (
         <Container>
             <Subtitle>Додаткові послуги:</Subtitle>
             <StyledFormGroup>
-                <div style={{display: 'flex', }}>
-                <FormControlLabel control={<StyledCheckbox
-                    value={selectedSealing}
-                    checked={selectedSealing}
-                    onChange={handleSealing}
-                    disabled={typeof oneProductPrice === "string"}
-                    sx={{
-                        color: yellow[800],
-                        '&.Mui-checked': {
+                <Group>
+                    <FormControlLabel control={<StyledCheckbox
+                        value={selectedSealing}
+                        checked={selectedSealing}
+                        onChange={handleSealing}
+                        disabled={typeof oneProductPrice === "string"}
+                        sx={{
                             color: yellow[800],
-                        },
-                    }} />} label="Герметизація" />
-                <p>{`+ ${sealingPrice} грн`}</p>
-                    {/* <FormHelperText>{`+ ${sealingPrice} грн`}</FormHelperText> */}
-                    </div>
+                            '&.Mui-checked': {
+                                color: yellow[800],
+                            },
+                        }} />} label="Герметизація" />
+                    <ExtraPrice>{`+ ${sealingPrice} грн`}</ExtraPrice>
+                </Group>
                 {holder &&
-                    <div style={{display: 'flex', }}>
+                    <Group>
                         <FormControlLabel control={<StyledCheckbox
-                    onChange={handleHolder}
-                    checked={selectedHolder}
-                    value={selectedHolder}
-                    disabled={typeof oneProductPrice === "string"}
-                    sx={{
-                        color: yellow[800],
-                        '&.Mui-checked': {
-                            color: yellow[800],
-                        },
-                    }} />} label="Використовувати холдери" />
-                        <p>{`+ ${holderPrice} грн`}</p>
-                    </div>
-                }
-                
-               
+                            onChange={handleHolder}
+                            checked={selectedHolder}
+                            value={selectedHolder}
+                            disabled={typeof oneProductPrice === "string"}
+                            sx={{
+                                color: yellow[800],
+                                '&.Mui-checked': {
+                                    color: yellow[800],
+                                },
+                            }} />} label="Використовувати холдери" />
+                        <ExtraPrice>{`+ ${holderPrice} грн`}</ExtraPrice>
+                    </Group>}
             </StyledFormGroup>
         </Container>
     );
