22f2b5764f03834f9d9e0599788a1fb5a429c3e7
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-03-22 14:38:41 +0200

    fix_css

--- src/components/CardPage/Card.styled.js
+++ src/components/CardPage/Card.styled.js
@@ -54,7 +54,6 @@ export const ImageBig = styled.img`
   height: 200px;
   object-fit: cover;
   border-radius: 12px;
-  /* box-shadow: 0px 2px 2px 0px rgba(0, 0, 0, 0.15); */
 
   @media screen and (min-width: 1280px) {
     min-width: 518px;
@@ -68,7 +67,6 @@ export const ImageSmall = styled.img`
   height: 40px;
   object-fit: cover;
   border-radius: 6px;
-  /* box-shadow: 0px 2px 2px 0px rgba(0, 0, 0, 0.15); */
 
   @media screen and (min-width: 1280px) {
     width: 80px;
@@ -95,11 +93,19 @@ export const Image = styled.img`
 
 export const SliderDiv = styled.div`
   width: 310px;
+  height: 200px;
+  margin: 0 auto;
   margin-bottom: 50px;
 
   .slick-dots li {
     width: 50px;
+    height: 40px;
     border-radius: 6px;
+  }
+
+  .slick-dots {
+    height: 40px;
+    bottom: -38px;
   }
 
   .slick-list {
@@ -117,6 +123,7 @@ export const SliderDiv = styled.div`
     margin-bottom: 95px;
 
     .slick-dots {
+      height: 60px;
       bottom: -65px;
     }
 
