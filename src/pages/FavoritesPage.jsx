76ff1d02873511f4db92fced9b2774175368b65c
Va7ul <vas7ul@gmail.com>
2024-03-24 11:13:24 +0200

    Create FavoritesPage.jsx

---
+++ src/pages/FavoritesPage.jsx
@@ -0,0 +1,12 @@
+import { SideBarNav } from 'components/Shared/SideBarNav/SideBarNav';
+
+const FavoritesPage = () => {
+  return (
+    <>
+      <SideBarNav />
+      <div>FavoritesPage</div>
+    </>
+  );
+};
+
+export default FavoritesPage;
