0cbe347268e16b6572b690298addfc49cb15de8e
Va7ul <vas7ul@gmail.com>
2024-04-03 23:40:01 +0300

    change paddings

--- src/components/Shared/ProductsSection/ProductsSection.styled.js
+++ src/components/Shared/ProductsSection/ProductsSection.styled.js
@@ -2,21 +2,21 @@ import styled from 'styled-components';
 import { getSectionBackground } from 'styles/GlobalStyled';
 
 export const StyledContainer = styled.div`
-  padding: 15px 20px;
+  padding: 20px;
   background: ${getSectionBackground};
 
   @media screen and (min-width: 1280px) {
-    padding: 40px 110px 30px;
+    padding: 50px 110px;
   }
 `;
 
 export const StyledTitle = styled.h2`
   font-size: 16px;
   font-weight: 600;
-  margin-bottom: 15px;
+  margin-bottom: 10px;
 
   @media screen and (min-width: 1280px) {
     margin-bottom: 30px;
-    font-size: 32px;
+    font-size: 40px;
   }
 `;
