f440f425b1eb125e8eda803a93f07dc3b16381c8
1evanter <rita.oleksenkoo@gmail.com>
2024-04-05 21:50:02 +0300

    add Link to checkout page

--- src/components/Shared/CartModal/FilledCart/FilledCart.styled.js
+++ src/components/Shared/CartModal/FilledCart/FilledCart.styled.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { gradientTransitionBtn } from 'styles/GlobalStyled';
 
@@ -45,6 +46,31 @@ export const BtnWrap = styled.div`
 `;
 
 export const Button = styled.button`
+  padding-top: 12px;
+  padding-bottom: 12px;
+  border-radius: 6px;
+  font-size: 12px;
+  font-weight: 400;
+  width: 149px;
+
+  box-shadow: 0px 4px 12.3px 0px rgba(0, 0, 0, 0.05);
+  background: ${props => props.theme.colors.gradient};
+
+  position: relative;
+  z-index: 0;
+  ${gradientTransitionBtn}
+  &::after {
+    border-radius: 6px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    padding: 15px 8px;
+    font-size: 15px;
+    width: 188px;
+  }
+`;
+
+export const StyledLink = styled(Link)`
   padding-top: 12px;
   padding-bottom: 12px;
   border-radius: 6px;
