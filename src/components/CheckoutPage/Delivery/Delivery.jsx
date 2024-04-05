598549ffb3694034ef682612dcbf34dd6443749d
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-04-05 23:33:24 +0300

    added_css

--- src/components/CheckoutPage/Delivery/Delivery.jsx
+++ src/components/CheckoutPage/Delivery/Delivery.jsx
@@ -5,23 +5,25 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import { styled } from '@mui/material/styles';
 import { themeMUI } from '../../../styles/GlobalStyled';
-// import FormLabel from '@mui/material/FormLabel';
 import { yellow } from '@mui/material/colors';
-import { Button, ButtonBox, Title, NPTitle, NPIcon} from './Delivery.styled';
+import { Button, ButtonBox, Title, NPTitle, NPText, NPIcon} from './Delivery.styled';
 import sprite from '../../../assets/images/sprite.svg';
 
 
 const StyledRadioGroup = styled(RadioGroup)({
     gap: '5px',
-     [themeMUI.breakpoints.only('desktop')]: {
-            gap: '20px',
-        },
     '& .MuiButtonBase-root': {
-        padding: '0px 10px 0px 0px'
+        padding: '0px 5px 0px 0px',
+        [themeMUI.breakpoints.only('desktop')]: {
+            padding: '0px 10px 0px 0px',
+        },
     },
     '& .MuiFormControlLabel-root': {
         margin: 0,
         alignItems: "flex-start",
+          [themeMUI.breakpoints.only('desktop')]: {
+            alignItems: "center",
+        },
     },
     '& .MuiTypography-root': {
         fontSize: '10px',
@@ -35,15 +37,11 @@ const StyledRadio = styled(Radio)({
     '& .MuiSvgIcon-root': {
         width: '16px',
         height: '16px',
-        [themeMUI.breakpoints.only('desktop')]: {
-            width: '30px',
-            height: '30px',
-        },
     },
 });
 
 export const Delivery = () => {
-  const [value, setValue] = useState('female');
+    const [value, setValue] = useState('female');
 
     const handleChange = (event) => {
         setValue(event.target.value);
@@ -54,51 +52,50 @@ export const Delivery = () => {
             <Title>Спосіб доставки</Title>
             <ButtonBox>
                 <Button>
-                     <NPTitle>
-              <NPIcon>
-                <use href={`${sprite}#nova_poshta`}></use>
-              </NPIcon>
-              Нова пошта
-            </NPTitle>
+                    <NPTitle>
+                        <NPIcon>
+                            <use href={`${sprite}#nova_poshta`}></use>
+                        </NPIcon>
+                        <NPText>Нова пошта</NPText> 
+                    </NPTitle>
                 </Button>
                 <Button>Самовивіз</Button>
             </ButtonBox>
             <Title>Спосіб оплати</Title>
-   <FormControl>
-      {/* <FormLabel id="demo-controlled-radio-buttons-group">Спосіб оплати</FormLabel> */}
-      <StyledRadioGroup
+            <FormControl>
+                <StyledRadioGroup
                     aria-labelledby="demo-controlled-radio-buttons-group"
                     name="controlled-radio-buttons-group"
                     value={value}
                     onChange={handleChange}
-      >
+                >
                     <FormControlLabel value="card" control={<StyledRadio
-                         sx={{
-                        color: yellow[700],
-                        '&.Mui-checked': {
-                            color: yellow[800],
-                        },
-                    }}
+                        sx={{
+                            color: yellow[700],
+                            '&.Mui-checked': {
+                                color: yellow[800],
+                            },
+                        }}
                     />} label="Картою по реквізитах фізичних осіб" />
                     <FormControlLabel value="account" control={<StyledRadio
-                     sx={{
-                        color: yellow[700],
-                        '&.Mui-checked': {
-                            color: yellow[800],
-                        },
-                    }}
+                        sx={{
+                            color: yellow[700],
+                            '&.Mui-checked': {
+                                color: yellow[800],
+                            },
+                        }}
                     />} label="Рахунок для юридичних осіб або ФОП" />
                     <FormControlLabel value="cash" control={<StyledRadio
-                     sx={{
-                        color: yellow[700],
-                        '&.Mui-checked': {
-                            color: yellow[800],
-                        },
-                    }}
+                        sx={{
+                            color: yellow[700],
+                            '&.Mui-checked': {
+                                color: yellow[800],
+                            },
+                        }}
                     />} label="Накладений платіж (при замовленні індивідуальної збірки, передоплата 20%)" />
                     
-      </StyledRadioGroup>
-    </FormControl>
+                </StyledRadioGroup>
+            </FormControl>
         </div>
     );
 };
