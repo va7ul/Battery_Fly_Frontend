48e633f34de4bbf7b944171feb9889e9d8b08b5b
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-04-05 15:01:11 +0300

    created_ Delivery_for_Checkout

---
+++ src/components/CheckoutPage/Delivery/Delivery.jsx
@@ -0,0 +1,10 @@
+export const Delivery = () => {
+
+    return (
+        <div>
+            <p>Спосіб доставки</p>
+            <button>Нова пошта</button>
+            <button>Самовивіз</button>
+        </div>
+    );
+};
