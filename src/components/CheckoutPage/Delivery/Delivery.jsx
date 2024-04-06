3a1e1d8e3d5fbccb0471599e146fe60403c7b290
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-04-06 18:35:44 +0300

    added_styles

--- src/components/CheckoutPage/Delivery/Delivery.jsx
+++ src/components/CheckoutPage/Delivery/Delivery.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
+import LocationCityIcon from '@mui/icons-material/LocationCity';
 import { styled } from '@mui/material/styles';
 import { themeMUI } from '../../../styles/GlobalStyled';
 import { yellow } from '@mui/material/colors';
-import { Button, ButtonBox, Title, NPTitle, NPText, NPIcon} from './Delivery.styled';
+import { Button, ButtonBox, Title, NPTitle, NPText, NPIcon, BoxAddress, BoxIcon, Text, Address, OrderButton, Box} from './Delivery.styled';
 import sprite from '../../../assets/images/sprite.svg';
 
 
@@ -22,7 +24,7 @@ const StyledRadioGroup = styled(RadioGroup)({
         margin: 0,
         alignItems: "flex-start",
           [themeMUI.breakpoints.only('desktop')]: {
-            alignItems: "center",
+            alignItems: "baseline",
         },
     },
     '& .MuiTypography-root': {
@@ -37,10 +39,16 @@ const StyledRadio = styled(Radio)({
     '& .MuiSvgIcon-root': {
         width: '16px',
         height: '16px',
+        [themeMUI.breakpoints.only('desktop')]: {
+            width: '18px',
+            height: '18px',
+        },
     },
 });
 
 export const Delivery = () => {
+    const mobileVersion = useMediaQuery({ query: '(max-width:1279px)' });
+
     const [value, setValue] = useState('female');
 
     const handleChange = (event) => {
@@ -56,46 +64,65 @@ export const Delivery = () => {
                         <NPIcon>
                             <use href={`${sprite}#nova_poshta`}></use>
                         </NPIcon>
-                        <NPText>Нова пошта</NPText> 
+                        <NPText>Нова пошта</NPText>
                     </NPTitle>
                 </Button>
                 <Button>Самовивіз</Button>
             </ButtonBox>
+            <BoxAddress>
+                <Text>Адреса для самовивозу:</Text>
+                <BoxIcon>
+                    {mobileVersion ? <LocationCityIcon sx={{ fontSize: 22 }} /> : <LocationCityIcon sx={{ fontSize: 30 }} />}
+                    <Address
+                        href="https://maps.app.goo.gl/HVQb7UZCPnmQ73356"
+                        target="_blank"
+                        rel="noopener noreferrer nofollow"
+                    >
+                        м. Львів, вул. Зелена, 109
+                    </Address>
+                </BoxIcon>
+                
+            </BoxAddress>
             <Title>Спосіб оплати</Title>
-            <FormControl>
-                <StyledRadioGroup
-                    aria-labelledby="demo-controlled-radio-buttons-group"
-                    name="controlled-radio-buttons-group"
-                    value={value}
-                    onChange={handleChange}
-                >
-                    <FormControlLabel value="card" control={<StyledRadio
-                        sx={{
-                            color: yellow[700],
-                            '&.Mui-checked': {
-                                color: yellow[800],
-                            },
-                        }}
-                    />} label="Картою по реквізитах фізичних осіб" />
-                    <FormControlLabel value="account" control={<StyledRadio
-                        sx={{
-                            color: yellow[700],
-                            '&.Mui-checked': {
-                                color: yellow[800],
-                            },
-                        }}
-                    />} label="Рахунок для юридичних осіб або ФОП" />
-                    <FormControlLabel value="cash" control={<StyledRadio
-                        sx={{
-                            color: yellow[700],
-                            '&.Mui-checked': {
-                                color: yellow[800],
-                            },
-                        }}
-                    />} label="Накладений платіж (при замовленні індивідуальної збірки, передоплата 20%)" />
+            <Box>
+                <FormControl>
+                    <StyledRadioGroup
+                        aria-labelledby="demo-controlled-radio-buttons-group"
+                        name="controlled-radio-buttons-group"
+                        value={value}
+                        onChange={handleChange}
+                    >
+                        <FormControlLabel value="card" control={<StyledRadio
+                            sx={{
+                                color: yellow[700],
+                                '&.Mui-checked': {
+                                    color: yellow[800],
+                                },
+                            }}
+                        />} label="Картою по реквізитах фізичних осіб" />
+                        <FormControlLabel value="account" control={<StyledRadio
+                            sx={{
+                                color: yellow[700],
+                                '&.Mui-checked': {
+                                    color: yellow[800],
+                                },
+                            }}
+                        />} label="Рахунок для юридичних осіб або ФОП" />
+                        <FormControlLabel value="cash" control={<StyledRadio
+                            sx={{
+                                color: yellow[700],
+                                '&.Mui-checked': {
+                                    color: yellow[800],
+                                },
+                            }}
+                        />} label="Накладений платіж (при замовленні індивідуальної збірки, передоплата 20%)" />
                     
-                </StyledRadioGroup>
-            </FormControl>
+                    </StyledRadioGroup>
+                </FormControl>
+                <OrderButton>
+                    <div>Оформити замовлення</div>
+                </OrderButton>
+            </Box>
         </div>
     );
 };
