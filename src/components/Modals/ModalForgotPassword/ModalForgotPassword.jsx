43b11d669883b6f240af558dffa060839d498aa9
NatalyaMaterinskaya <natulka-91@ukr.net>
2024-04-08 02:13:21 +0300

    Added TextField component

--- src/components/Modals/ModalForgotPassword/ModalForgotPassword.jsx
+++ src/components/Modals/ModalForgotPassword/ModalForgotPassword.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import ReactModal from 'react-modal';
 import { useFormik } from 'formik';
-import { TextField, styled } from '@mui/material';
 import { forgotPasswordSchema } from '../../../common/schemas/forgotPasswordSchema';
 import { ModalAgree } from '../SharedComponent/ModalAgree/ModalAgree';
 import { TextAgree } from '../SharedComponent/Text/Text';
+import { Field } from '../SharedComponent/TextField/TextField';
 import { Btn, StyledForm, Text } from './ModalForgotPassword.styled';
 
 const customStyles = {
@@ -27,82 +27,6 @@ const customStyles = {
   },
 };
 
-const Field = styled(TextField)(({ theme }) => ({
-  '& .MuiOutlinedInput-notchedOutline': {
-    borderColor: theme.palette.text.primary,
-    borderRadius: '6px',
-    [theme.breakpoints.up('desktop')]: {
-      borderRadius: '8px',
-    },
-  },
-  '& .MuiOutlinedInput-input': {
-    height: '28px',
-    padding: '0px 0px',
-    [theme.breakpoints.up('desktop')]: {
-      height: '51px',
-    },
-  },
-  '& .MuiFormLabel-root': {
-    fontFamily: theme.typography.fontFamily,
-    fontSize: '10px',
-    fontWeight: theme.typography.fontWeightMedium,
-    color: theme.palette.text.primary,
-    top: '-8px',
-    [theme.breakpoints.up('desktop')]: {
-      fontSize: '14px',
-      top: '0px',
-    },
-    '&.Mui-focused': {
-      fontSize: '10px',
-      color: theme.palette.text.primary,
-      transform: 'translate(10px, -4px) scale(1)',
-      [theme.breakpoints.up('desktop')]: {
-        fontSize: '14px',
-        transform: 'translate(10px, -18px) scale(1)',
-      },
-    },
-  },
-  '& .MuiFormLabel-filled': {
-    transform: 'translate(10px, -4px) scale(1)',
-    [theme.breakpoints.up('desktop')]: {
-      transform: 'translate(10px, -18px) scale(1)',
-    },
-  },
-  '& .MuiInputBase-root': {
-    height: '28px',
-    padding: '0px 8px',
-    fontFamily: theme.typography.fontFamily,
-    fontSize: '10px',
-    [theme.breakpoints.up('desktop')]: {
-      height: '51px',
-      padding: '0px 12px',
-      fontSize: '14px',
-    },
-    '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-      border: `2px solid ${theme.palette.secondary.main}`,
-    },
-  },
-  '& .MuiFormLabel-root.Mui-error': {
-    fontFamily: theme.typography.fontFamily,
-    fontSize: '10px',
-    fontWeight: '500',
-    color: theme.palette.text.primary,
-    transform: 'translate(10px, -6px)',
-    [theme.breakpoints.up('desktop')]: {
-      fontSize: '14px',
-      transform: 'translate(10px, -20px)',
-    },
-  },
-  '& .MuiFormHelperText-root.Mui-error': {
-    fontFamily: theme.typography.fontFamily,
-    fontSize: '8px',
-    fontWeight: '500',
-    color: 'rgba(255, 0, 0, 1)',
-    [theme.breakpoints.up('desktop')]: {
-      fontSize: '12px',
-    },
-  },
-}));
 
 ReactModal.setAppElement('#modal-root');
 
--- src/components/Modals/ModalSignUpSignIn/SignInForm/SignInForm.jsx
+++ src/components/Modals/ModalSignUpSignIn/SignInForm/SignInForm.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useFormik } from 'formik';
-import { IconButton, InputAdornment, TextField, styled } from '@mui/material';
+import { IconButton, InputAdornment } from '@mui/material';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 import { signInSchema } from '../../../../common/schemas/signInSchema';
 import { login } from '../../../../redux/user/userOperations';
@@ -17,83 +17,8 @@ import { ModalAgree } from 'components/Modals/SharedComponent/ModalAgree/ModalAg
 import { TextAgree } from 'components/Modals/SharedComponent/Text/Text';
 import { useAuth } from 'hooks/useAuth';
 import { changeErrorStatus } from '../../../../redux/user/userSlice';
+import { Field } from 'components/Modals/SharedComponent/TextField/TextField';
 
-const Field = styled(TextField)(({ theme }) => ({
-  '& .MuiOutlinedInput-notchedOutline': {
-    borderColor: theme.palette.text.primary,
-    borderRadius: '6px',
-    [theme.breakpoints.up('desktop')]: {
-      borderRadius: '8px',
-    },
-  },
-  '& .MuiOutlinedInput-input': {
-    height: '28px',
-    padding: '0px 0px',
-    [theme.breakpoints.up('desktop')]: {
-      height: '51px',
-    },
-  },
-  '& .MuiFormLabel-root': {
-    fontFamily: theme.typography.fontFamily,
-    fontSize: '10px',
-    fontWeight: theme.typography.fontWeightMedium,
-    color: theme.palette.text.primary,
-    top: '-8px',
-    [theme.breakpoints.up('desktop')]: {
-      fontSize: '14px',
-      top: '0px',
-    },
-    '&.Mui-focused': {
-      fontSize: '10px',
-      color: theme.palette.text.primary,
-      transform: 'translate(10px, -4px) scale(1)',
-      [theme.breakpoints.up('desktop')]: {
-        fontSize: '14px',
-        transform: 'translate(10px, -18px) scale(1)',
-      },
-    },
-  },
-  '& .MuiFormLabel-filled': {
-    transform: 'translate(10px, -4px) scale(1)',
-    [theme.breakpoints.up('desktop')]: {
-      transform: 'translate(10px, -18px) scale(1)',
-    },
-  },
-  '& .MuiInputBase-root': {
-    height: '28px',
-    padding: '0px 8px',
-    fontFamily: theme.typography.fontFamily,
-    fontSize: '10px',
-    [theme.breakpoints.up('desktop')]: {
-      height: '51px',
-      padding: '0px 12px',
-      fontSize: '14px',
-    },
-    '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-      border: `2px solid ${theme.palette.secondary.main}`,
-    },
-  },
-  '& .MuiFormLabel-root.Mui-error': {
-    fontFamily: theme.typography.fontFamily,
-    fontSize: '10px',
-    fontWeight: '500',
-    color: theme.palette.text.primary,
-    transform: 'translate(10px, -6px)',
-    [theme.breakpoints.up('desktop')]: {
-      fontSize: '14px',
-      transform: 'translate(10px, -20px)',
-    },
-  },
-  '& .MuiFormHelperText-root.Mui-error': {
-    fontFamily: theme.typography.fontFamily,
-    fontSize: '8px',
-    fontWeight: '500',
-    color: 'rgba(255, 0, 0, 1)',
-    [theme.breakpoints.up('desktop')]: {
-      fontSize: '12px',
-    },
-  },
-}));
 
 export const SignInForm = ({ handleCloseSignUpSignInModal }) => {
   const [showPassword, setShowPassword] = useState(false);
