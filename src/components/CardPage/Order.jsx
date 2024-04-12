837272bc7b02695862ac5220ecb1cfeebd9347b6
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-04-12 11:27:11 +0300

    fix

--- src/components/CardPage/Order.jsx
+++ src/components/CardPage/Order.jsx
@@ -32,116 +32,116 @@ import {
 } from './Card.styled';
 
 export const Order = () => {
-  const [isModalQuickOrderOpen, setIsModalQuickOrderOpen] = useState(false);
+    const [isModalQuickOrderOpen, setIsModalQuickOrderOpen] = useState(false);
 
-  const handleOpenQuickOrderModal = () => {
-    setIsModalQuickOrderOpen(true);
-    document.body.style.overflow = 'hidden';
-  };
-  const handleCloseQuickOrderModal = () => {
-    setIsModalQuickOrderOpen(false);
-    document.body.style.overflow = 'unset';
-  };
+    const handleOpenQuickOrderModal = () => {
+        setIsModalQuickOrderOpen(true);
+        document.body.style.overflow = 'hidden';
+    };
+    const handleCloseQuickOrderModal = () => {
+        setIsModalQuickOrderOpen(false);
+        document.body.style.overflow = 'unset';
+    };
 
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const product = useSelector(selectOneProduct);
-  const { name, codeOfGood, price, quantity, capacity, capacityKey, discount, sale } = product;
-  const oneProductPrice = useSelector(selectOneProductPrice);
-  const selectedSealing = useSelector(selectSelectedSealing);
-  const selectedHolder = useSelector(selectSelectedHolder);
-  const quantityOrders = useSelector(selectQuantityOrders);
-  const sealingPrice = useSelector(selectSealingPrice);
-  const holderPrice = useSelector(selectHolderPrice);
-  const priceWithSale = useSelector(selectPriceWithSale);
+    const product = useSelector(selectOneProduct);
+    const { name, codeOfGood, price, quantity, capacity, capacityKey, discount, sale } = product;
+    const oneProductPrice = useSelector(selectOneProductPrice);
+    const selectedSealing = useSelector(selectSelectedSealing);
+    const selectedHolder = useSelector(selectSelectedHolder);
+    const quantityOrders = useSelector(selectQuantityOrders);
+    const sealingPrice = useSelector(selectSealingPrice);
+    const holderPrice = useSelector(selectHolderPrice);
+    const priceWithSale = useSelector(selectPriceWithSale);
 
-  useEffect(() => {
-    dispatch(setSealingPrice(100 * quantityOrders));
-  }, [dispatch, quantityOrders]);
+    useEffect(() => {
+        dispatch(setSealingPrice(100 * quantityOrders));
+    }, [dispatch, quantityOrders]);
 
-  useEffect(() => {
-    let holderCost = 0;
-    if (capacity) {
-      holderCost = capacity[capacityKey]?.holder * 2 || 0;
-    }
-    dispatch(setHolderPrice(holderCost * quantityOrders));
-  }, [dispatch, quantityOrders, capacityKey, capacity]);
+    useEffect(() => {
+        let holderCost = 0;
+        if (capacity) {
+            holderCost = capacity[capacityKey]?.holder * 2 || 0;
+        }
+        dispatch(setHolderPrice(holderCost * quantityOrders));
+    }, [dispatch, quantityOrders, capacityKey, capacity]);
 
-  // useEffect(() => {
+    // useEffect(() => {
 
-  //     if (selectedSealing && selectedHolder) {
-  //         dispatch(setPrice(quantityOrders * oneProductPrice + sealingPrice + holderPrice));
-  //         dispatch(setPriceWithSale(quantityOrders * oneProductPrice * ((100 - discount) / 100) + sealingPrice + holderPrice));
-  //         return;
-  //     }
+    //     if (selectedSealing && selectedHolder) {
+    //         dispatch(setPrice(quantityOrders * oneProductPrice + sealingPrice + holderPrice));
+    //         dispatch(setPriceWithSale(quantityOrders * oneProductPrice * ((100 - discount) / 100) + sealingPrice + holderPrice));
+    //         return;
+    //     }
 
-  //     if (selectedSealing) {
-  //         dispatch(setPrice(quantityOrders * oneProductPrice + sealingPrice));
-  //         dispatch(setPriceWithSale(quantityOrders * oneProductPrice * ((100 - discount) / 100) + sealingPrice));
-  //         return;
-  //     }
+    //     if (selectedSealing) {
+    //         dispatch(setPrice(quantityOrders * oneProductPrice + sealingPrice));
+    //         dispatch(setPriceWithSale(quantityOrders * oneProductPrice * ((100 - discount) / 100) + sealingPrice));
+    //         return;
+    //     }
 
-  //     if (selectedHolder) {
-  //         dispatch(setPrice(quantityOrders * oneProductPrice + holderPrice));
-  //         dispatch(setPriceWithSale(quantityOrders * oneProductPrice * ((100 - discount) / 100) + holderPrice));
-  //         return;
-  //     }
+    //     if (selectedHolder) {
+    //         dispatch(setPrice(quantityOrders * oneProductPrice + holderPrice));
+    //         dispatch(setPriceWithSale(quantityOrders * oneProductPrice * ((100 - discount) / 100) + holderPrice));
+    //         return;
+    //     }
 
-  //     dispatch(setPrice(quantityOrders * oneProductPrice));
-  //     if (typeof oneProductPrice === 'string') {
-  //         return;
-  //     }
-  //     dispatch(setPriceWithSale(quantityOrders * oneProductPrice * ((100 - discount) / 100)));
+    //     dispatch(setPrice(quantityOrders * oneProductPrice));
+    //     if (typeof oneProductPrice === 'string') {
+    //         return;
+    //     }
+    //     dispatch(setPriceWithSale(quantityOrders * oneProductPrice * ((100 - discount) / 100)));
 
-  // }, [dispatch, selectedSealing, holderPrice, oneProductPrice, selectedHolder, quantityOrders, sealingPrice, discount]);
+    // }, [dispatch, selectedSealing, holderPrice, oneProductPrice, selectedHolder, quantityOrders, sealingPrice, discount]);
 
-  useEffect(() => {
-    if (typeof oneProductPrice === 'string') {
-      return;
-    }
+    useEffect(() => {
+        if (typeof oneProductPrice === 'string') {
+            return;
+        }
 
-    let totalPrice = quantityOrders * oneProductPrice;
-    let totalPriceWithSale = totalPrice * ((100 - discount) / 100);
+        let totalPrice = quantityOrders * oneProductPrice;
+        let totalPriceWithSale = totalPrice * ((100 - discount) / 100);
 
-    if (selectedSealing) {
-      totalPrice += sealingPrice;
-      totalPriceWithSale += sealingPrice;
-    }
+        if (selectedSealing) {
+            totalPrice += sealingPrice;
+            totalPriceWithSale += sealingPrice;
+        }
 
-    if (selectedHolder) {
-      totalPrice += holderPrice;
-      totalPriceWithSale += holderPrice;
-    }
+        if (selectedHolder) {
+            totalPrice += holderPrice;
+            totalPriceWithSale += holderPrice;
+        }
 
-    dispatch(setPrice(totalPrice));
-    dispatch(setPriceWithSale(totalPriceWithSale));
-  }, [
-    dispatch,
-    selectedSealing,
-    selectedHolder,
-    quantityOrders,
-    oneProductPrice,
-    sealingPrice,
-    holderPrice,
-    discount,
-  ]);
+        dispatch(setPrice(totalPrice));
+        dispatch(setPriceWithSale(totalPriceWithSale));
+    }, [
+        dispatch,
+        selectedSealing,
+        selectedHolder,
+        quantityOrders,
+        oneProductPrice,
+        sealingPrice,
+        holderPrice,
+        discount,
+    ]);
 
-  const plusOne = () => {
-    if (quantityOrders < quantity) {
-      dispatch(setQuantityOrders(quantityOrders + 1));
-    } else {
-      toast.success(`Максимальна кількість в наявності: ${quantity} шт`, {
-        id: 'clipboard',
-        duration: 4000,
-      });
-    }
-  };
+    const plusOne = () => {
+        if (quantityOrders < quantity) {
+            dispatch(setQuantityOrders(quantityOrders + 1));
+        } else {
+            toast.success(`Максимальна кількість в наявності: ${quantity} шт`, {
+                id: 'clipboard',
+                duration: 4000,
+            });
+        }
+    };
 
-  const minusOne = () => {
-    if (quantityOrders > 1) {
-      dispatch(setQuantityOrders(quantityOrders - 1));
-    }
-  };
+    const minusOne = () => {
+        if (quantityOrders > 1) {
+            dispatch(setQuantityOrders(quantityOrders - 1));
+        }
+    };
 
     const setValue = e => {
         Number(e.target.value.replace(/[^\d.]*/g, '')
@@ -161,79 +161,79 @@ export const Order = () => {
         }
     };
 
-  const minValue = () => {
-    if (quantityOrders === '') {
-      dispatch(setQuantityOrders(1));
-    }
-  };
+    const minValue = () => {
+        if (quantityOrders === '') {
+            dispatch(setQuantityOrders(1));
+        }
+    };
 
-  const addToBasket = () => {
-    dispatch(
-      addItem({
-        ...product,
-        price: sale ? priceWithSale / quantityOrders : price / quantityOrders,
-        capacityKey: capacityKey || '',
-        selectedSealing,
-        selectedHolder,
-        quantityOrdered: quantityOrders,
-        totalPrice: sale ? priceWithSale : price,
-      })
-    );
-    toast.success(`Товар доданий до кошика`, {
-      id: 'clipboard',
-      duration: 4000,
-    });
-  };
+    const addToBasket = () => {
+        dispatch(
+            addItem({
+                ...product,
+                price: sale ? priceWithSale / quantityOrders : price / quantityOrders,
+                capacityKey: capacityKey || '',
+                selectedSealing,
+                selectedHolder,
+                quantityOrdered: quantityOrders,
+                totalPrice: sale ? priceWithSale : price,
+            })
+        );
+        toast.success(`Товар доданий до кошика`, {
+            id: 'clipboard',
+            duration: 4000,
+        });
+    };
 
-  return (
-    <OrderBox>
-      <CounterBox>
-        <Button
-          onClick={minusOne}
-          disabled={typeof oneProductPrice === 'string'}
-        >
-          <div>
-            <FaMinus />
-          </div>
-        </Button>
-        <Input
-          min="1"
-          max={quantity}
-          onBlur={minValue}
-          onChange={setValue}
-          value={quantityOrders}
-          disabled={typeof oneProductPrice === 'string'}
-        ></Input>
-        <Button
-          onClick={plusOne}
-          disabled={typeof oneProductPrice === 'string'}
-        >
-          <div>
-            <FaPlus />
-          </div>
-        </Button>
-      </CounterBox>
-      <ButtonBox>
-        <BasketButton
-          disabled={typeof oneProductPrice === 'string'}
-          onClick={addToBasket}
-        >
-          <div>В кошик</div>
-        </BasketButton>
-        <OrderButton
-          onClick={handleOpenQuickOrderModal}
-        >
-          <div>Швидке замовлення</div>
-        </OrderButton>
-      </ButtonBox>
-      <div>
-        <Toaster position="top-center" reverseOrder={false} />
-      </div>
-      <ModalQuickOrder
-        product={{ name, codeOfGood, priceWithSale }}
-        isModalQuickOrderOpen={isModalQuickOrderOpen}
-        handleCloseQuickOrderModal={handleCloseQuickOrderModal}
-      />
-    </OrderBox>
-  )
+    return (
+        <OrderBox>
+            <CounterBox>
+                <Button
+                    onClick={minusOne}
+                    disabled={typeof oneProductPrice === 'string'}
+                >
+                    <div>
+                        <FaMinus />
+                    </div>
+                </Button>
+                <Input
+                    min="1"
+                    max={quantity}
+                    onBlur={minValue}
+                    onChange={setValue}
+                    value={quantityOrders}
+                    disabled={typeof oneProductPrice === 'string'}
+                ></Input>
+                <Button
+                    onClick={plusOne}
+                    disabled={typeof oneProductPrice === 'string'}
+                >
+                    <div>
+                        <FaPlus />
+                    </div>
+                </Button>
+            </CounterBox>
+            <ButtonBox>
+                <BasketButton
+                    disabled={typeof oneProductPrice === 'string'}
+                    onClick={addToBasket}
+                >
+                    <div>В кошик</div>
+                </BasketButton>
+                <OrderButton
+                    onClick={handleOpenQuickOrderModal}
+                >
+                    <div>Швидке замовлення</div>
+                </OrderButton>
+            </ButtonBox>
+            <div>
+                <Toaster position="top-center" reverseOrder={false} />
+            </div>
+            <ModalQuickOrder
+                product={{ name, codeOfGood, priceWithSale }}
+                isModalQuickOrderOpen={isModalQuickOrderOpen}
+                handleCloseQuickOrderModal={handleCloseQuickOrderModal}
+            />
+        </OrderBox>
+    )
 };
