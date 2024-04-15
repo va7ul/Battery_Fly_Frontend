ebc4d74a123698ba5eeae90906cf131a7f54f084
Va7ul <vas7ul@gmail.com>
2024-04-15 20:30:34 +0300

    create one order details template

---
+++ src/components/OrdersHistoryPage/OrdersHistoryListEl/OrderDetailsList/OrderDetailsList.jsx
@@ -0,0 +1,13 @@
+export const OrderDetailsList = () => {
+  //   const data = {};
+
+  return (
+    <>
+      {/* {data.map(el => (
+        <li key={el.number}>
+          <OrdersHistoryListEl el={el} />
+        </li>
+      ))} */}
+    </>
+  );
+};
