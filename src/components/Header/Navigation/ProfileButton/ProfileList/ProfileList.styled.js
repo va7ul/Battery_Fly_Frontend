8cce7dcd2eb66bdcd31ee15538a1c11cdd9fe678
1evanter <rita.oleksenkoo@gmail.com>
2024-04-16 00:40:12 +0300

    Create ProfileList.styled.js

---
+++ src/components/Header/Navigation/ProfileButton/ProfileList/ProfileList.styled.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Item = styled.li`
+  width: 100%;
+  font-size: 13px;
+  font-weight: 600;
+  padding-top: 2px;
+  padding-bottom: 2px;
+
+  &:hover {
+    color: ${props => props.theme.colors.hoverColor};
+  }
+
+  &:not(:last-child) {
+    border-bottom: 1px solid ${props => props.theme.colors.textPrimary};
+  }
+`;
+
+export const Button = styled.button`
+  font-size: 13px;
+  font-weight: 600;
+  background-color: transparent;
+  border: none;
+  &:hover {
+    color: ${props => props.theme.colors.hoverColor};
+  }
+`;
