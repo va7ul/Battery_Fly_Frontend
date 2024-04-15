bf1f514e5ad73a5d2f59454c3eb159e2b3624859
1evanter <rita.oleksenkoo@gmail.com>
2024-04-16 00:39:59 +0300

    add Menu to ProfileButton

--- src/components/Header/Navigation/ProfileButton/ProfileButton.jsx
+++ src/components/Header/Navigation/ProfileButton/ProfileButton.jsx
@@ -1,15 +1,26 @@
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-import { Wrapper, Button, ArrowButton } from './ProfileButton.styled';
+import { Wrapper, StyledLink, ArrowButton } from './ProfileButton.styled';
 import sprite from '../../../../assets/images/sprite.svg';
 import { useSelector } from 'react-redux';
 import { selectUserData } from '../../../../redux/user/userSelectors';
+import { Menu } from '@mui/material';
+import { useState } from 'react';
+import { ProfileList } from './ProfileList/ProfileList';
 
 export const ProfileButton = () => {
   const userData = useSelector(selectUserData);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <Wrapper>
-      <Button type="button">
+      <StyledLink to="/profile">
         <AccountCircleOutlinedIcon
           sx={{
             color: 'text.primary',
@@ -18,12 +29,39 @@ export const ProfileButton = () => {
           }}
         />
         {userData.firstName}
-      </Button>
-      <ArrowButton type="button">
+      </StyledLink>
+      <ArrowButton
+        handleopen={anchorEl}
+        type="button"
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
         <svg>
           <use href={`${sprite}#arrow-left`}></use>
         </svg>
       </ArrowButton>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+        sx={{
+          '& .MuiMenu-paper': {
+            bgcolor: 'svgColor.main',
+            width: '130px',
+            p: '2px 5px',
+            borderRadius: '6px',
+          },
+        }}
+      >
+        <ProfileList handleClose={handleClose} />
+      </Menu>
     </Wrapper>
   );
 };
