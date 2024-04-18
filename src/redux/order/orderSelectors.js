3b4be96ab61d69738bcfeba8a4cf8b39915ec1cf
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-04-18 03:45:06 +0300

    added_ selectWarehouse

--- src/redux/order/orderSelectors.js
+++ src/redux/order/orderSelectors.js
@@ -4,3 +4,4 @@ export const selectError = state => state.order.error;
 export const selectCities = state => state.order.delivery.cities;
 export const selectWarehouses = state => state.order.delivery.warehouses;
 export const selectCity = state => state.order.delivery.city;
+export const selectWarehouse = state => state.order.delivery.warehouse;
