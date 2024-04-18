05684cf3d96a7b90e43acc06de5983d6866d31d9
1evanter <rita.oleksenkoo@gmail.com>
2024-04-18 23:19:12 +0300

    remove FeedbackButton & modal

--- src/components/Header/MobileToolBar/MobileToolBar.jsx
+++ src/components/Header/MobileToolBar/MobileToolBar.jsx
@@ -1,31 +1,19 @@
 import { Wrapper, Button } from './MobileToolBar.styled';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-import LoginIcon from '@mui/icons-material/Login';
-import { FeedBackButton } from 'components/Shared/FeedbackButton/FeedbackButton';
 import { CartIcon } from 'components/Shared/CartIcon';
 import { FavoriteIcon } from 'components/Shared/FavoriteIcon';
 import { useState } from 'react';
-import { ModalFeedback } from 'components/Modals/ModalFeedback/ModalFeedback';
 import { CartModal } from 'components/Shared/CartModal/CartModal';
 import { useDispatch, useSelector } from 'react-redux';
 import { setCartOpen } from '../../../redux/menu/menuSlice';
 import { ModalSignUpSignIn } from '../../Modals/ModalSignUpSignIn/ModalSignUpSignIn';
 import { Link } from 'react-router-dom';
 import { selectIsLoggedIn } from '../../../redux/user/userSelectors';
+import { ProfileButton } from '../ProfileButton/ProfileButton';
+import { LoginButton } from '../LoginButton/LoginButton';
 
 export const MobileToolBar = () => {
-  const [isModalFeedbackOpen, setIsModalFeedbackOpen] = useState(false);
   const dispatch = useDispatch();
   const isLoggedIn = useSelector(selectIsLoggedIn);
-
-  const handleOpenFeedbackModal = () => {
-    setIsModalFeedbackOpen(true);
-    document.body.style.overflow = 'hidden';
-  };
-  const handleCloseFeedbackModal = () => {
-    setIsModalFeedbackOpen(false);
-    document.body.style.overflow = 'unset';
-  };
 
   const [isModalSignUpSignInOpen, setIsModalSignUpSignInOpen] = useState(false);
 
@@ -44,11 +32,6 @@ export const MobileToolBar = () => {
 
   return (
     <Wrapper>
-      <FeedBackButton handleOpenModal={handleOpenFeedbackModal} />
-      <ModalFeedback
-        isModalFeedbackOpen={isModalFeedbackOpen}
-        handleCloseFeedbackModal={handleCloseFeedbackModal}
-      />
       <Button type="button" onClick={openDrawer}>
         <CartIcon />
       </Button>
@@ -62,29 +45,11 @@ export const MobileToolBar = () => {
       />
 
       {isLoggedIn ? (
-        <Button type="button">
-          <AccountCircleOutlinedIcon
-            sx={{
-              color: 'background.paper',
-              width: '24px',
-              ' &:hover': {
-                color: 'secondary.main',
-              },
-            }}
-          />
-        </Button>
+        <ProfileButton />
       ) : (
-        <Button type="button" onClick={handleOpenSignUpSignInModal}>
-          <LoginIcon
-            sx={{
-              color: 'background.paper',
-              width: '24px',
-              ' &:hover': {
-                color: 'secondary.main',
-              },
-            }}
-          />
-        </Button>
+        <LoginButton
+          handleOpenSignUpSighInModal={handleOpenSignUpSignInModal}
+        />
       )}
     </Wrapper>
   );
