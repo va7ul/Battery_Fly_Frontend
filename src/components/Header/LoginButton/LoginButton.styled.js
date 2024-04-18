1db84bdb6d19ced35d6ff2ab415b1eebf8beffd0
1evanter <rita.oleksenkoo@gmail.com>
2024-04-18 23:20:03 +0300

    LoginButton mobile styles

--- src/components/Header/LoginButton/LoginButton.styled.js
+++ src/components/Header/LoginButton/LoginButton.styled.js
@@ -4,15 +4,23 @@ export const Button = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 112px;
+
+  min-width: 100px;
   gap: 4px;
-  padding: 6px 20px;
-  border-radius: 10px;
-  font-size: 14px;
+  padding: 3px;
+  border-radius: 6px;
+  font-size: 10px;
   font-weight: 600;
   background-color: ${props => props.theme.colors.secondary};
 
   &:hover {
     background-color: ${props => props.theme.colors.hoverColor};
+  }
+
+  @media screen and (min-width: 1280px) {
+    font-size: 14px;
+    min-width: 112px;
+    padding: 6px 20px;
+    border-radius: 10px;
   }
 `;
