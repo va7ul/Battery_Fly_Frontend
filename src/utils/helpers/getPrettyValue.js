4df4136404b47c6481ec2237ce5351695f0cfe6f
Va7ul <vas7ul@gmail.com>
2024-04-29 16:27:43 +0300

    add PrettyPrice function

--- src/utils/helpers/getPrettyValue.js
+++ src/utils/helpers/getPrettyValue.js
@@ -1,5 +1,5 @@
-export const getPrettyPrice = price => {
-  const prettyPriceArr = price.toString().split('');
+export const getPrettyValue = value => {
+  const prettyPriceArr = value.toString().split('');
   const firstLength = prettyPriceArr.length;
 
   for (let n = firstLength - 3; n >= 1; n -= 3) {
--- src/utils/helpers/index.js
+++ src/utils/helpers/index.js
@@ -3,5 +3,5 @@ export * from './getCategoryName';
 export * from './getDate';
 export * from './getNewPrice';
 export * from './getOrderInfo'; 
-export * from './getPrettyPrice'; 
+export * from './getPrettyValue'; 
 export * from './getProductList';
