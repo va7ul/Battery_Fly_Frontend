e0c124d0d6ab51801c03133d402790076af0d4cc
Va7ul <vas7ul@gmail.com>
2024-05-05 20:46:56 +0300

    Update Options.jsx

--- src/components/Print3DPage/Options/Options.jsx
+++ src/components/Print3DPage/Options/Options.jsx
@@ -1,29 +1,17 @@
+import { useSelector } from 'react-redux';
 import { CustomSelect } from './CustomSelect/CustomSelect';
+import { selectPrint3D } from '../../../redux/print3D/print3DSelectors';
 import { OptionsContainer, Subtitle } from './Options.styled';
 
 export const Options = () => {
-  const selectData = {
-    accuracy: ['100 мікрон', '200 мікрон', '300 мікрон'],
-    plactic: ['PLA', 'PET-G', 'ABC'],
-    color: [
-      'Чорний',
-      'Білий',
-      'Сірий',
-      'Червоний',
-      'Жовтий',
-      'Коричневий',
-      'Оранжевий',
-      'Зелений',
-      'Синій',
-    ],
-  };
+  const { accuracy, plactic, color } = useSelector(selectPrint3D);
 
   return (
     <OptionsContainer>
       <Subtitle>Параметри друку:</Subtitle>
-      <CustomSelect data={selectData.accuracy} placeholder={'Точність'} />
-      <CustomSelect data={selectData.plactic} placeholder={'Тип пластику'} />
-      <CustomSelect data={selectData.color} placeholder={'Колір'} />
+      <CustomSelect data={accuracy} placeholder={'Точність'} />
+      <CustomSelect data={plactic} placeholder={'Тип пластику'} />
+      <CustomSelect data={color} placeholder={'Колір'} />
     </OptionsContainer>
   );
 };
