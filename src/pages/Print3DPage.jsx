2af3190a5a58ab9e11ce2e3564cd2861187c9a30
Va7ul <vas7ul@gmail.com>
2024-05-05 20:46:40 +0300

    Update Print3DPage.jsx

--- src/pages/Print3DPage.jsx
+++ src/pages/Print3DPage.jsx
@@ -1,21 +1,26 @@
-// import { useDispatch, useSelector } from 'react-redux';
-// import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getPrint3D } from '../redux/print3D/print3DOperations';
+import {
+  selectPrint3DIsLoading,
+  selectPrint3DError,
+} from '../redux/print3D/print3DSelectors';
+import { CustomLoader } from 'components/Shared/CustomLoader/CustomLoader';
 import { Print3D } from 'components/Print3DPage/Print3D';
 
 const Print3DPage = () => {
-  // const dispatch = useDispatch();
-  // const isLoading = useSelector(selectOneProductIsLoading);
-  // const error = useSelector(selectOneProductError);
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectPrint3DIsLoading);
+  const error = useSelector(selectPrint3DError);
 
-  // useEffect(() => {
-  //   dispatch(get3DPrint());
-  // }, [dispatch]);
+  useEffect(() => {
+    dispatch(getPrint3D());
+  }, [dispatch]);
 
   return (
     <>
-      {/* {isLoading && <CustomLoader />} */}
-      {/* {!isLoading && !error && <Print3D />} */}
-      <Print3D />
+      {isLoading && <CustomLoader />}
+      {!isLoading && !error && <Print3D />}
     </>
   );
 };
