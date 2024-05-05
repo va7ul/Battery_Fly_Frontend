c247d20202ede77889bfea3a5416ff549e8e9211
Va7ul <vas7ul@gmail.com>
2024-05-05 20:46:54 +0300

    Update Description.jsx

--- src/components/Print3DPage/Description/Description.jsx
+++ src/components/Print3DPage/Description/Description.jsx
@@ -1,9 +1,9 @@
-// import { useSelector } from 'react-redux';
-
+import { useSelector } from 'react-redux';
+import { selectPrint3D } from '../../../redux/print3D/print3DSelectors';
 import { Container, Desc, Item, Subtitle } from './Description.styled';
 
-export const Description = ({ description }) => {
-  //   const { description } = useSelector(select3DPrint);
+export const Description = () => {
+  const { description } = useSelector(selectPrint3D);
 
   const newDescription = description.split(';');
 
