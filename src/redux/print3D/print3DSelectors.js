f1816b7ef1bce1cce3c1e363705bf3ade09c1e8d
Va7ul <vas7ul@gmail.com>
2024-05-05 20:46:29 +0300

    Create print3DSelectors.js

---
+++ src/redux/print3D/print3DSelectors.js
@@ -0,0 +1,3 @@
+export const selectPrint3DIsLoading = state => state.print3D.isLoading;
+export const selectPrint3DError = state => state.print3D.error;
+export const selectPrint3D = state => state.print3D.result;
