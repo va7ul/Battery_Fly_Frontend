6aaedee298bf01902fd4490952677c67e52c984b
1evanter <rita.oleksenkoo@gmail.com>
2024-05-10 21:50:57 +0300

    change logout button

--- src/components/Header/ProfileButton/ProfileList/ProfileList.jsx
+++ src/components/Header/ProfileButton/ProfileList/ProfileList.jsx
@@ -1,33 +1,24 @@
-import { List, Item, Button, StyledLink } from './ProfileList.styled';
-import { useDispatch } from 'react-redux';
-import { logOut } from '../../../../redux/user/userOperations';
+import { List, Item, StyledLink } from './ProfileList.styled';
+import { LogoutButton } from 'components/Shared/LogoutButton/LogoutButton';
 
 export const ProfileList = ({ handleClose }) => {
-  const dispatch = useDispatch();
-
-  const logout = () => {
-    dispatch(logOut());
-  };
-
   return (
-    <List onClick={handleClose}>
-      <Item>
+    <List>
+      <Item onClick={handleClose}>
         <StyledLink to="/profile" style={{ paddingRight: '32%' }}>
           Мій профіль
         </StyledLink>
       </Item>
-      <Item>
+      <Item onClick={handleClose}>
         <StyledLink to="/orders" style={{ paddingRight: 0 }}>
           Історія замовлень
         </StyledLink>
       </Item>
-      <Item>
+      <Item onClick={handleClose}>
         <StyledLink to="/favorites">Обране</StyledLink>
       </Item>
       <Item>
-        <Button type="button" onClick={logout}>
-          Вийти
-        </Button>
+        <LogoutButton />
       </Item>
     </List>
   );
