aae88b05833e339420db16ff7bf2b468066ad529
Va7ul <vas7ul@gmail.com>
2024-05-14 20:27:52 +0300

    Update Print3D.jsx

--- src/components/Print3DPage/Print3D.jsx
+++ src/components/Print3DPage/Print3D.jsx
@@ -1,7 +1,12 @@
 import { useSelector } from 'react-redux';
 import { useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
-import { selectPrint3D } from '../../redux/print3D/print3DSelectors';
+import {
+  selectPrint3D,
+  selectedAccuracy,
+  selectedPlactic,
+  selectedColor,
+} from '../../redux/print3D/print3DSelectors';
 import { ProductPhoto } from 'components/Shared/ProductPhoto/ProductPhoto';
 import { Modal3DPrint } from 'components/Modals/Modal3DPrint/Modal3DPrint';
 import {
@@ -11,19 +16,38 @@ import {
   Case,
   Title,
   OrderButton,
-  FileWrapper,
-  UploadButton,
+  StyledInput,
 } from './Print3D.styled';
 import { ProductInformation } from 'components/Shared/ProductInformation/ProductInformation';
 import { Description } from './Description/Description';
 import { Options } from './Options/Options';
 import { PriceList } from './PriceList/PriceList';
+// import { add3DPrintOrder } from '../../redux/print3D/print3DOperations';
 
 export const Print3D = () => {
   const desktopVersion = useMediaQuery({ query: '(min-width:1280px)' });
   const [isModal3DPrintOpen, setIsModal3DPrintOpen] = useState(false);
+  const [file, setFile] = useState('');
+  // const dispatch = useDispatch();
 
   const handleOpen3DPrintModal = () => {
+    // let formData = new FormData();
+    // formData.append('file', file);
+
+    // for (const value of formData.values()) {
+    //   console.log(value);
+    // }  //це для відображення полів, які відправляєш
+
+    // dispatch(add3DPrintOrder(formData));
+
+    if (!accuracy || !plactic || !color) {
+      return window.alert('Оберіть параметри друку!');
+    }
+
+    if (!file) {
+      return window.alert('Прикріпіть файл для друку!');
+    }
+
     setIsModal3DPrintOpen(true);
     document.body.style.overflow = 'hidden';
   };
@@ -33,6 +57,13 @@ export const Print3D = () => {
   };
 
   const { name, image, information } = useSelector(selectPrint3D);
+  const accuracy = useSelector(selectedAccuracy);
+  const plactic = useSelector(selectedPlactic);
+  const color = useSelector(selectedColor);
+
+  const attachFile = e => {
+    setFile(e.currentTarget.files[0]);
+  };
 
   return (
     <Wrapper>
@@ -42,10 +73,14 @@ export const Print3D = () => {
           <ProductPhoto name={name} image={image} />
           <Description />
           <Options />
-          <FileWrapper>
-            <UploadButton>Вибрати файл</UploadButton>
-            <div>Файл не вибрано</div>
-          </FileWrapper>
+          <form>
+            <StyledInput
+              id="file"
+              name="file"
+              type="file"
+              onChange={attachFile}
+            />
+          </form>
           <OrderButton onClick={handleOpen3DPrintModal}>
             Замовити друк
           </OrderButton>
@@ -57,11 +92,18 @@ export const Print3D = () => {
             <Title>{name}</Title>
             <Description />
             <Options />
-            <FileWrapper>
-              <UploadButton>Вибрати файл</UploadButton>
-              <div>Файл не вибрано</div>
-            </FileWrapper>
-            <OrderButton onClick={handleOpen3DPrintModal}>
+            <form>
+              <StyledInput
+                id="file"
+                name="file"
+                type="file"
+                onChange={attachFile}
+              />
+            </form>
+            <OrderButton
+              // disabled={!accuracy || !plactic || !color || !file}
+              onClick={handleOpen3DPrintModal}
+            >
               Замовити друк
             </OrderButton>
           </Box>
