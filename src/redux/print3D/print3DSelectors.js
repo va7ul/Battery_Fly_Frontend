c2f9effe961708313b0f4e235263dd33f4623d1a
Va7ul <vas7ul@gmail.com>
2024-05-14 19:58:15 +0300

    connect print3d to backend routes

--- src/redux/print3D/print3DSelectors.js
+++ src/redux/print3D/print3DSelectors.js
@@ -1,3 +1,6 @@
 export const selectPrint3DIsLoading = state => state.print3D.isLoading;
 export const selectPrint3DError = state => state.print3D.error;
 export const selectPrint3D = state => state.print3D.result;
+export const selectedAccuracy = state => state.print3D.selectedAccuracy;
+export const selectedPlactic = state => state.print3D.selectedPlactic;
+export const selectedColor = state => state.print3D.selectedColor;
