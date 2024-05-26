c13c29766d832777eac8bec9b15d42f196079be5
NatalyaMaterinskaya <natulka-91@ukr.net>
2024-05-26 17:01:07 +0300

    Added toasts

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 import { persistor, store } from './redux/store.js';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle, theme } from './styles/GlobalStyled';
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <ThemeProvider theme={theme}>
             <App />
             <GlobalStyle />
+            <Toaster/>
           </ThemeProvider>
         </BrowserRouter>
       </PersistGate>
