c6748d48ac30484d5ce687af441c712ef43ae6b2
1evanter <rita.oleksenkoo@gmail.com>
2024-06-17 20:27:45 +0300

    change logo's size

--- src/components/Header/Logo/Logo.styled.js
+++ src/components/Header/Logo/Logo.styled.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 export const StyledLogo = styled.img`
-  width: 90px;
+  width: 100%;
+  padding-right: 10px;
 
   @media screen and (min-width: 1280px) {
     width: 177px;
