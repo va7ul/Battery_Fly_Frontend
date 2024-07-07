6c7b1a242473e52825f25a5904895100b47f4013
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-07-07 17:37:24 +0300

    fixed_bags

--- src/components/Footer/Confidential.jsx
+++ src/components/Footer/Confidential.jsx
@@ -1,16 +1,10 @@
-import { Box, TextDate, Text } from './Footer.styled';
+import { Box, TextDate } from './Footer.styled';
 
 export const Confidential = () => {
   return (
     <Box>
       <TextDate>&copy; 2024 batteryfly.ua | by Dream Team </TextDate>
-      <Text>
-        <a href="https://mma.in.ua/pol%D1%96tika-konf%D1%96denc%D1%96jnost%D1%96?gad_source=1&gclid=Cj0KCQjw7ZO0BhDYARIsAFttkChK0_mRoURgOqYmtE7sNFL03SH_vKkcn4FtKnsXaaNjenxNqAUphRIaAnGCEALw_wcB"
-          target='_blank'
-          rel="noopener noreferrer nofollow"
-        >
-          Політика конфіденційності</a>
-      </Text>
+      <TextDate>Політика конфіденційності</TextDate>
     </Box>
   );
 };
