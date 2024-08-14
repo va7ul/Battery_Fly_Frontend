85eb2af7c7ba524ce2562e76b4e492c7007b5f2c
NatalyaMaterinskaya <natulka-91@ukr.net>
2024-08-14 12:52:12 +0300

    Update userSelectors.ts

--- src/redux/user/userSelectors.ts
+++ src/redux/user/userSelectors.ts
@@ -9,7 +9,6 @@ export const selectErrorStatus = (state: RootState) => state.user.errorStatus;
 export const selectVerifiedEmail = (state: RootState) => state.user.verifiedEmail;
 export const selectMessageOfSuccessfulRequest = (state: RootState) =>
   state.user.messageOfSuccessfulRequest;
-// export const selectOrders = (state: RootState) => state.user.orders;
 export const selectDelivery = (state: RootState) => state.user.delivery;
 export const selectFavorites = (state: RootState) => state.user.favorites;
 export const selectOrdersHistory = (state: RootState) =>
