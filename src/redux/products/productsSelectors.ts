e256a040acc12a46f028b1ddd245756a6eba9533
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-08-16 16:56:13 +0300

    transition_TS_to_productSelectors

--- src/redux/products/productsSelectors.ts
+++ src/redux/products/productsSelectors.ts
@@ -1,21 +1,21 @@
 import { RootState } from 'redux/store';
 
-export const selectOneProductIsLoading = state =>
+export const selectOneProductIsLoading = (state: RootState) =>
   state.products.oneProduct.isLoading;
-export const selectOneProductError = state => state.products.oneProduct.error;
-export const selectOneProduct = state => state.products.oneProduct.result;
-export const selectOneProductPrice = state =>
+export const selectOneProductError = (state: RootState) => state.products.oneProduct.error;
+export const selectOneProduct = (state: RootState) => state.products.oneProduct.result;
+export const selectOneProductPrice = (state: RootState) =>
   state.products.oneProduct.result.priceOneProduct;
-export const selectSelectedHolder = state =>
+export const selectSelectedHolder = (state: RootState) =>
   state.products.oneProduct.selectedHolder;
-export const selectSelectedSealing = state =>
+export const selectSelectedSealing = (state: RootState) =>
   state.products.oneProduct.selectedSealing;
-export const selectQuantityOrders = state =>
+export const selectQuantityOrders = (state: RootState) =>
   state.products.oneProduct.quantityOrders;
-export const selectSealingPrice = state =>
+export const selectSealingPrice = (state: RootState) =>
   state.products.oneProduct.sealingPrice;
-export const selectHolderPrice = state => state.products.oneProduct.holderPrice;
-export const selectPriceWithSale = state =>
+export const selectHolderPrice = (state: RootState) => state.products.oneProduct.holderPrice;
+export const selectPriceWithSale = (state: RootState) =>
   state.products.oneProduct.priceWithSale;
 
 export const selectAllProductsIsLoading = (state: RootState) =>
