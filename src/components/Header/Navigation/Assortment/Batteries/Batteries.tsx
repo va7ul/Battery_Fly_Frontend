d9fe8501ad31cdd8435f47c34b66de869f75a2d8
1evanter <rita.oleksenkoo@gmail.com>
2024-09-03 16:37:55 +0300

    Batteries

--- src/components/Header/Navigation/Assortment/Batteries/Batteries.tsx
+++ src/components/Header/Navigation/Assortment/Batteries/Batteries.tsx
@@ -1,6 +1,6 @@
 import { useMediaQuery } from 'react-responsive';
 import { MobileBatteries } from './MobileBatteries/MobileBatteries';
-import { Item } from '../../Assortment/AssortmentList/ListItem/ListItem.styled';
+import { Item } from '../AssortmentList/ListItem/ListItem.styled';
 import { DesktopBatteries } from './DesktopBatteries/DesktopBatteries';
 
 export const Batteries = () => {
