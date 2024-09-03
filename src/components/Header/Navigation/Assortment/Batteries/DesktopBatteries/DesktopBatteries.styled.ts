94e9b1781213873ecb975ac5307c64ee526db8b5
1evanter <rita.oleksenkoo@gmail.com>
2024-09-03 16:43:02 +0300

    DesktopBatteries

--- src/components/Header/Navigation/Assortment/Batteries/DesktopBatteries/DesktopBatteries.styled.ts
+++ src/components/Header/Navigation/Assortment/Batteries/DesktopBatteries/DesktopBatteries.styled.ts
@@ -1,8 +1,7 @@
 import { getRotation } from 'styles/globalStyles';
-import {  ButtonProps } from '@mui/material';
 import styled from 'styled-components';
 
-interface BatteryButtonProps extends ButtonProps {
+type BatteryButtonProps = {
   handleOpen?: HTMLElement | null;
 }
 
@@ -30,28 +29,3 @@ export const BatteryButton = styled.button<BatteryButtonProps>`
     transform: rotate(270deg) scale(1.4);
   }
 `;
-
-// export const BatteryButton = styled.button`
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   background-color: transparent;
-//   outline: none;
-//   border: none;
-//   margin-right: 20px;
-//   margin-left: auto;
-
-//   & svg {
-//     transform: ${getRotation};
-//     width: 16px;
-//     height: 16px;
-//     fill: transparent;
-//     stroke: ${props => props.theme.colors.textPrimary};
-//     transition: ${props => props.theme.transition.main};
-//   }
-
-//   &:hover svg {
-//     stroke: ${props => props.theme.colors.hoverColor};
-//     transform: rotate(270deg) scale(1.4);
-//   }
-// `;
