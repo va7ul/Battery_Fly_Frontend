5627e089941171c660c6c5de79134699f71e2015
1evanter <rita.oleksenkoo@gmail.com>
2024-09-04 20:33:45 +0300

    CartIcon

--- src/components/Shared/CartIcon.tsx
+++ src/components/Shared/CartIcon.tsx
@@ -1,10 +1,10 @@
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import { useSelector } from 'react-redux';
 import { selectItems } from '../../redux/basket/basketSelectors';
 import { BadgeComponent } from './Badge';
+import { useTypedSelector } from '../../redux/hooks';
 
 export const CartIcon = () => {
-  const products = useSelector(selectItems);
+  const products = useTypedSelector(selectItems);
   return (
     <BadgeComponent value={products.length}>
       <ShoppingCartOutlinedIcon
