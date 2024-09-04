4f22be13587e5d34c5a2af24b38e9495c1bacb7b
1evanter <rita.oleksenkoo@gmail.com>
2024-09-04 21:01:05 +0300

    FeedbackButton

--- src/components/Shared/FeedbackButton/FeedbackButton.jsx
+++
@@ -1,9 +0,0 @@
-import { Button } from './FeedbackButton.styled';
-
-export const FeedBackButton = ({ handleOpenModal }) => {
-  return (
-    <Button type="button" onClick={handleOpenModal}>
-      <div>Зв'язатись</div>
-    </Button>
-  );
-};
