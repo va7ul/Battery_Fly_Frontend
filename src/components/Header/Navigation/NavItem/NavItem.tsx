2942d0b9868cd1a2356efd86d643f019a6266a90
1evanter <rita.oleksenkoo@gmail.com>
2024-09-19 20:38:45 +0300

    NavItem

--- src/components/Header/Navigation/NavItem/NavItem.tsx
+++ src/components/Header/Navigation/NavItem/NavItem.tsx
@@ -1,8 +1,10 @@
 import { useMediaQuery } from 'react-responsive';
 import { Item, StyledLink } from './NavItem.styled';
 import { HopeIconMobile } from 'components/Shared/HopeIconMobile/HopeIconMobile';
+import { FC } from 'react';
+import { ListItemProps } from '../../../../@types/header.types';
 
-export const NavItem = ({ title, page }) => {
+export const NavItem: FC<ListItemProps> = ({ title, page }) => {
   const mobileVersion = useMediaQuery({ query: '(max-width:1279px)' });
 
   return (
